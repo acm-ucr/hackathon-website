f2c7b38c7f23035457f588b0b18608095777113a
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-07-06 10:53:58 +0800

    create Title Component

--- src/app/admin/participants/page.js
+++ src/app/admin/participants/page.js
@@ -3,11 +3,15 @@ import Filters from "@/app/components/Filters";
 import Participants from "@/app/components/Participants";
 import Toolbar from "@/app/components/Toolbar";
 import React from "react";
+import Title from "@/app/components/Title";
 
 const ParticipantsPage = () => {
   return (
     <div className="w-11/12">
-      <Filters />
+      <div className="flex">
+        <Title title="Participants" />
+        <Filters />
+      </div>
       <Toolbar />
       <Participants />
     </div>
