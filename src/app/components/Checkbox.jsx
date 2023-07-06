1373c57e02bfbdfd5feb3d75d19682f0c6ab56a1
menthy-wu <wumengxuan0728@gmail.com>
2023-07-06 14:28:37 -0700

    checkbox

---
+++ src/app/components/Checkbox.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BsCheckLg } from "react-icons/bs";
+
+const Checkbox = ({ onClick, selected, setSelected }) => {
+  return (
+    <div
+      className={`w-5 h-5 hover:cursor-pointer ${
+        selected ? "bg-hackathon-blue-100" : "bg-hackathon-gray"
+      } flex items-center justify-center`}
+      onClick={() => {
+        setSelected(!selected);
+        onClick();
+      }}
+    >
+      {selected && <BsCheckLg className="text-white text-lg" />}
+    </div>
+  );
+};
+
+export default Checkbox;
