d4a3e5e36b5f39df53384256343d99312f9a1c8f
Divyank Shah <divyank.shah.2016@gmail.com>
2023-07-24 19:01:41 -0400

    simplify

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -3,11 +3,12 @@ import Image from "next/image.js";
 import LOGO from "../../public/LOGO.png";
 
 const ProfileHeader = ({ horizontal = true, email, name }) => {
-  let flexDirection = "";
-  horizontal ? (flexDirection = "flex-row") : (flexDirection = "flex-col");
-
   return (
-    <div className={`flex ${flexDirection} items-center justify-center m-auto`}>
+    <div
+      className={`flex ${
+        horizontal ? "flex-row" : "flex-col"
+      } items-center justify-center m-auto`}
+    >
       <Image src={LOGO} className="w-32 mr-4 rounded-full overflow-hidden" />
       <div className="flex flex-col justify-center items-center">
         <p className="text-2xl font-bold">{name}</p>
