5c2b3e814e02076f3b029aa6c534d4310a38cdcb
fardinzam <fardinzaman51@gmail.com>
2023-07-26 12:35:40 -0700

    removed homepage toolbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,18 @@
 "use client";
-import Toolbar from "../components/Toolbar";
+// import Toolbar from "../components/Toolbar";
 const Home = () => {
-  const handleTagClick = (tagText) => {
-    console.log(`Tag "${tagText}" clicked!`);
-  };
+  // const handleTagClick = (tagText) => {
+  //   console.log(`Tag "${tagText}" clicked!`);
+  // };
 
-  const tags = [
-    { text: "winner", name: "" },
-    { text: "disqualified", name: "" },
-    { text: "pending", name: "" },
-  ];
+  // const tags = [
+  //   { text: "winner", name: "" },
+  //   { text: "disqualified", name: "" },
+  //   { text: "pending", name: "" },
+  // ];
 
-  return <Toolbar tags={tags} onClick={handleTagClick} />;
+  // return <Toolbar tags={tags} onClick={handleTagClick} />;
+  return <h1 style={{}}>HOME PAGE PLACEHOLDER</h1>;
 };
 
 export default Home;
