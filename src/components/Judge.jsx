ae74d517e8cfb35ed7b895794aa623f2c7a1552e
Divyank Shah <divyank.shah.2016@gmail.com>
2023-07-26 00:19:46 -0400

    judges logic

--- src/components/Judge.jsx
+++ src/components/Judge.jsx
@@ -17,23 +17,47 @@ const colors = {
   },
 };
 
-const Judge = ({ type, name, email }) => {
-  const toggle = false;
-  const dummy = () => {
-    return 0;
+const Judge = ({
+  uid,
+  type,
+  name,
+  email,
+  selected,
+  filteredJudges,
+  setFilteredJudges,
+}) => {
+  const handleSelect = () => {
+    if (!selected) {
+      setFilteredJudges(
+        filteredJudges.map((a) => {
+          if (a.uid === uid) {
+            a.selected = true;
+          }
+          return a;
+        })
+      );
+    } else {
+      setFilteredTeams(
+        setFilteredJudges.map((a) => {
+          if (a.uid === uid) {
+            a.selected = false;
+          }
+          return a;
+        })
+      );
+    }
   };
-  const color = colors[type] || {};
-  const { bg, text } = color;
+
   return (
     <Card className="flex flex-row items-center !rounded-none border-t-0">
       <div className=" ml-4 w-[11%]">
-        <Checkbox onClick={dummy} toggle={toggle} />
+        <Checkbox onClick={handleSelect} toggle={selected} />
       </div>
 
       <div className="text-xl w-[34%] my-2">{name}</div>
       <div className="w-[28%]">
         <div
-          className={`${bg} ${text} px-4 text-xl rounded-full w-fit bg-opacity-20`}
+          className={`${colors[type].bg} ${colors[type].text} px-4 text-xl rounded-full w-fit bg-opacity-20`}
         >
           <div>{type}</div>
         </div>
