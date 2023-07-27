5a507ab9f03a8f136bac7a3e52f3d625afa441e3
fardinzam <fardinzaman51@gmail.com>
2023-07-26 17:43:14 -0700

    removed the commented code

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,5 @@
 "use client";
-// import Toolbar from "../components/Toolbar";
 const Home = () => {
-  // const handleTagClick = (tagText) => {
-  //   console.log(`Tag "${tagText}" clicked!`);
-  // };
-
-  // const tags = [
-  //   { text: "winner", name: "" },
-  //   { text: "disqualified", name: "" },
-  //   { text: "pending", name: "" },
-  // ];
-
-  // return <Toolbar tags={tags} onClick={handleTagClick} />;
   return <h1 style={{}}>HOME PAGE PLACEHOLDER</h1>;
 };
 
