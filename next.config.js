690e6c1089c846acb7d77712a873b702123c92ab
SafeDuck <schopra579@gmail.com>
2023-08-14 21:07:25 -0700

    Pull session name, email, id, image

--- next.config.js
+++ next.config.js
@@ -19,6 +19,17 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+    ],
+  },
+  
 };
 
 module.exports = nextConfig;
--- src/components/User/Header.jsx
+++ src/components/User/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image.js";
-import LOGO from "../../../public/LOGO.png";
+
+import { useSession } from "next-auth/react";
 
 const Header = ({ horizontal = true }) => {
-  const name = "Menthy Wu";
-  const email = "menthyiscool@gmail.com";
+  const { data: session} = useSession();
+
   return (
     <div
       className={`flex ${
@@ -12,13 +13,15 @@ const Header = ({ horizontal = true }) => {
       } items-center justify-center`}
     >
       <Image
-        src={LOGO}
+        src={session.user.image}  
+        width={125}
+        height={125}
         alt="Picture of user's profile"
         className="w-32 mr-4 rounded-full overflow-hidden"
       />
       <div className="align-left">
-        <p className="text-2xl font-bold mb-0">{name}</p>
-        <p className="text-base">{email}</p>
+        <p className="text-2xl font-bold mb-0">{session.user.name}</p>
+        <p className="text-base">{session.user.email}</p>
       </div>
     </div>
   );
