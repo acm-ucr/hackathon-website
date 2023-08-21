cd7f1be830a26bf9c5204e1674570766ab1f0fb0
menthy-wu <wumengxuan0728@gmail.com>
2023-08-21 11:40:31 -0700

    rounded

--- src/components/Admin/Tag.jsx
+++ src/components/Admin/Tag.jsx
@@ -47,7 +47,7 @@ const Tag = ({ color = "gray", text, onClick }) => {
     <div
       className={`${colors[color].background} ${colors[color].text} ${
         onClick && `hover:cursor-pointer ${colors[color].hover}`
-      } whitespace-nowrap px-2 py-0.5 rounded-full text-xs md:text-sm w-fit m-0`}
+      } whitespace-nowrap px-2 py-0.5 rounded text-xs md:text-sm w-fit m-0`}
       onClick={onClick}
     >
       {text}
