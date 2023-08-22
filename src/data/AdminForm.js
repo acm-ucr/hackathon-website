4c0c60db50a8d5bef96e1333eb18c061be9b38db
Anjaline Singh <sharansingh@Sharans-MacBook-Air.local>
2023-08-22 13:55:46 -0700

    I renamed the varible to Requirements.

--- src/data/AdminForm.js
+++ src/data/AdminForm.js
@@ -8,7 +8,7 @@ export const affiliations = [
   "marketing",
 ];
 
-export const requirementsData = {
+export const Requirements = {
   violation: {
     state: false,
     text: "You will be given access to private data that cannot be shared anywhere. If you violate this, you will be reported.",
