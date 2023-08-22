47d99cdc2db446d8569d392c1feb36fe001cefc5
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2023-08-21 22:46:13 -0700

    Extracted volunteer desccription and requirements

---
+++ src/data/Volunteers.js
@@ -0,0 +1,21 @@
+export const Description = {
+  hackathon: "Rose Hack",
+  about:
+    "women-centric hackathon founded by the female leaders of SWE and WINC",
+  date: "January 14-15, 2023",
+  role: "Volunteer",
+  responsibilities:
+    "in helping run the entire event. Responsibilities include helping set up, tech support, clean up, distributing swag, etc",
+  email: "rosehackucr@gmail.com",
+};
+
+export const Requirements = {
+  photography: {
+    state: false,
+    text: "I agree to photograph.",
+  },
+  inPerson: {
+    state: false,
+    text: "I understand that I will attend the event in person.",
+  },
+};
