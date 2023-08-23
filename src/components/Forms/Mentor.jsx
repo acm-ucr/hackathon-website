5ead08d710d23e8298a7d482ac78e21bb8f3be29
fardinzam <fardinzaman51@gmail.com>
2023-08-23 09:06:31 +0600

    Moved requirements into Mentors.jsx file

--- src/components/Forms/Mentor.jsx
+++ src/components/Forms/Mentor.jsx
@@ -10,20 +10,12 @@ import { Row, Col } from "react-bootstrap";
 import Button from "@/components/Forms/Button";
 import { Helper } from "@/data/User";
 import Textarea from "@/components/Forms/Textarea";
+import MENTOR_TEXT from "../../data/Mentors.jsx";
 
 const Mentor = () => {
   const [mentor, setMentor] = useState(Helper);
 
-  const [requirements, setRequirements] = useState({
-    photography: {
-      state: false,
-      text: "I agree to photograph.",
-    },
-    inPerson: {
-      state: false,
-      text: "I understand that I will attend the event in person.",
-    },
-  });
+  const [requirements, setRequirements] = useState(MENTOR_TEXT.requirements);
 
   const handleSubmit = () => {
     console.log(mentor);
@@ -45,23 +37,10 @@ const Mentor = () => {
       <div className="flex flex-col w-1/3 p-3 bg-white rounded-b-xl">
         <Row className="flex justify-center p-0 m-0">
           <Col xl={12}>
-            Hello! Rose Hack is coming January 14-15, 2023. Thank you for your
-            interest in Rose Hack, UC Riverside’s women-centric hackathon
-            founded by the female leaders of SWE and WINC!Mentors are essential
-            to our hackathon in helping guide our hackers with their projects.
-            Your experience and knowledge is valuable to those who need
-            assistance during the event. You are welcome to join remotely and
-            mentor over Discord/Zoom if that is convenient, but our event is
-            purely in-person in terms of hacking. Meals are also provided during
-            breakfast, lunch, and dinner times in person. If you are interested
-            in joining the Rose Hack Team as a mentor, please fill out this
-            quick interest form below!
+            {MENTOR_TEXT.welcome}
             <br />
             <br />
-            We also want to note that if you are mentoring at Rose Hack, you are
-            unable to participate as a hacker as well. If you have any other
-            questions please feel free to contact us on our socials or email us
-            at rosehackucr@gmail.com! :)
+            {MENTOR_TEXT.mentor_note}
           </Col>
           <Col xl={6}>
             <Input
