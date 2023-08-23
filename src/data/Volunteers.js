b64796693d859e447c349a37e76532cb6dd1fdbb
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2023-08-22 18:26:33 -0700

    Removed \n from data

--- src/data/Volunteers.js
+++ src/data/Volunteers.js
@@ -1,5 +1,5 @@
 export const Description = [
-  "Hello! Rose Hack is coming January 14-15, 2023. Thank you for your interest in Rose Hack, UC Riverside’s women-centric hackathon founded by the female leaders of SWE and WINC!Volunteers are essential to our hackathon in helping run the entire event. Responsibilities include helping set up, tech support, clean up, distributing swag, etc. Meals are also provided during breakfast, lunch, and dinner times. If you are interested in joining the Rose Hack Team as a volunteer, please fill out this quick interest form below! \n",
+  "Hello! Rose Hack is coming January 14-15, 2023. Thank you for your interest in Rose Hack, UC Riverside’s women-centric hackathon founded by the female leaders of SWE and WINC!Volunteers are essential to our hackathon in helping run the entire event. Responsibilities include helping set up, tech support, clean up, distributing swag, etc. Meals are also provided during breakfast, lunch, and dinner times. If you are interested in joining the Rose Hack Team as a volunteer, please fill out this quick interest form below!",
   "We also want to note that if you are volunteering at Rose Hack, you are unable to participate as a hacker as well. If you have any other questions please feel free to contact us on our socials or email us at rosehackucr@gmail.com! :)",
 ];
 export const Requirements = {
