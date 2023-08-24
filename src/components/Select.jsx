4db31c1a5e067b68e6aefae77648d3f6e3104dad
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2023-08-23 21:36:23 -0700

    Added scroll for dropdown menu

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -23,7 +23,7 @@ const Select = ({
           {user[field] || placeholder}
         </Dropdown.Toggle>
         {editable && (
-          <Dropdown.Menu className="w-full bg-hackathon-green-100 !border-none !rounded-none !p-0">
+          <Dropdown.Menu className="w-full bg-hackathon-green-100 !border-none !rounded-none !p-0 overflow-y-scroll max-h-[30vh]">
             {options.map((option, index) => (
               <Dropdown.Item
                 className=" hover:!bg-hackathon-green-200 !bg-hackathon-green-100 overflow-hidden"
