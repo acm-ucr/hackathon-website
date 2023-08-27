693c0cf54eb34b565b827cd8ea4600b3978a0212
Tiffany <twang032302@gmail.com>
2023-08-27 16:50:37 -0700

    adding proper affiliations to the calender in the admin page

--- src/data/Calendar.js
+++ src/data/Calendar.js
@@ -3,23 +3,43 @@ export const labels = {
     color: "yellow",
     background: "!bg-hackathon-green-200",
   },
-  operations: {
-    color: "yellow",
-    background: "!bg-hackathon-green-200",
-  },
-  webdev: {
-    color: "green",
+  director: {
+    color: "pink",
     background: "!bg-hackathon-green-200",
   },
   marketing: {
+    color: "green",
+    background: "!bg-hackathon-green-200",
+  },
+  operations: {
     color: "red",
     background: "!bg-hackathon-green-200",
   },
   sponsorship: {
+    color: "lightgreen",
+    background: "!bg-hackathon-green-200",
+  },
+  recruitment: {
+    color: "purple",
+    background: "!bg-hackathon-green-200",
+  },
+  software: {
+    color: "teal",
+    background: "!bg-hackathon-green-200",
+  },
+  UIUX: {
+    color: "grayblue",
+    background: "!bg-hackathon-green-200",
+  },
+  activities: {
     color: "purple",
     background: "!bg-hackathon-green-200",
   },
   workshop: {
+    color: "purple",
+    background: "!bg-hackathon-green-200",
+  },
+  general: {
     color: "purple",
     background: "!bg-hackathon-green-200",
   },
---
+++ src/data/Register.jsx
@@ -0,0 +1,9 @@
+photography: {
+    state: false,
+    text: "I agree to photography.",
+},
+
+inPerson: {
+    state: false,
+    text:"I understand that I will attend the event in person.",
+},
