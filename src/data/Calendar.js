da0356d01a3cfc63a961b823aa890ecc39b9f043
Tiffany <twang032302@gmail.com>
2023-08-29 14:08:48 -0700

    fixed color and background to corresponding colors

--- src/data/Calendar.js
+++ src/data/Calendar.js
@@ -1,11 +1,11 @@
 export const labels = {
   all: {
     color: "yellow",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-yellow-200",
   },
   director: {
     color: "pink",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-pink-200",
   },
   marketing: {
     color: "green",
@@ -13,34 +13,34 @@ export const labels = {
   },
   operations: {
     color: "red",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-red-200",
   },
   sponsorship: {
     color: "lightgreen",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-lightgreen-200",
   },
   recruitment: {
     color: "purple",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-purple-200",
   },
   software: {
     color: "teal",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-teal-200",
   },
   UIUX: {
     color: "grayblue",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-grayblue-200",
   },
   activities: {
     color: "purple",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-purple-200",
   },
   workshop: {
     color: "purple",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-purple-200",
   },
   general: {
     color: "purple",
-    background: "!bg-hackathon-green-200",
+    background: "!bg-hackathon-purple-200",
   },
 };
