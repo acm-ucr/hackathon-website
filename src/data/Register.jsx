eeff640077e00717f906958866c303ca1b92c0d4
Tiffany <twang032302@gmail.com>
2023-08-29 13:52:05 -0700

    create a register.jsx under the data folder to export register data

---
+++ src/data/Register.jsx
@@ -0,0 +1,9 @@
+export const requirphotography = {
+  state: false,
+  text: "I agree to photography.",
+};
+
+export const inPerson = {
+  state: false,
+  text: "I understand that I will attend the event in person.",
+};
