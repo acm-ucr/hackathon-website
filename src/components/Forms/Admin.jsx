1452962ece7e068cd605d378d2f4559af5eee0cc
sophiiiapapaya <swu241@ucr.edu>
2023-08-28 18:47:53 -0700

    added position options
    added options in data/AdminRequests and new field in components/Forms/Admin

--- src/components/Forms/Admin.jsx
+++ src/components/Forms/Admin.jsx
@@ -9,6 +9,7 @@ import { Row, Col } from "react-bootstrap";
 import Button from "./Button";
 import { affiliations } from "@/data/AdminRequest";
 import { requirement } from "@/data/AdminRequest";
+import { position } from "@/data/AdminRequest";
 
 const Admin = () => {
   const [user, setUser] = useState(User);
@@ -91,6 +92,15 @@ const Admin = () => {
               text="Affiliations"
               options={affiliations}
               field="affiliations"
+              user={user}
+              setUser={setUser}
+            />
+          </Col>
+          <Col xl={12}>
+            <Radio
+              text="Position"
+              options={position}
+              field="position"
               user={user}
               setUser={setUser}
             />
