c2ed316f9fce189b980fce883ea310eafd99fcbf
Tiffany <twang032302@gmail.com>
2023-08-29 19:57:35 -0700

    changed to an array with objects in

--- src/data/Register.jsx
+++ src/data/Register.jsx
@@ -1,9 +1,10 @@
-export const requirphotography = {
-  state: false,
-  text: "I agree to photography.",
-};
-
-export const inPerson = {
-  state: false,
-  text: "I understand that I will attend the event in person.",
+export const Registration = {
+  photography: {
+    state: false,
+    text: "I agree to photograph.",
+  },
+  inPerson: {
+    state: false,
+    text: "I understand that I will attend the event in person.",
+  },
 };
