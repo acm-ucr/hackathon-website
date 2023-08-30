5688772f5ee721dbffbf12ffe249f3d6250aa9be
Sharan Singh <sharan.singh0203@gmail.com>
2023-08-30 15:55:37 -0700

    fix

--- src/components/Admin/Toolbar.jsx
+++ src/components/Admin/Toolbar.jsx
@@ -141,6 +141,7 @@ const Toolbar = ({ input, setInput, tags, setObjects, objects, filters }) => {
           />
         )}
       </div>
+
       <button>
         <FaDownload
           size={22.5}
