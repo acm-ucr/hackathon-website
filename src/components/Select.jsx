a5a4c34457a6c5b968f829765a5eca30f77f646c
SafeDuck <schopra579@gmail.com>
2023-08-31 20:39:07 -0700

    Disable drop drop menu when not in edit mode

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -13,14 +13,25 @@ const Select = ({
     <div className="mt-3">
       <p className="mb-1">{title}</p>
       <Dropdown className="w-full m-0">
-        <Dropdown.Toggle
-          id="dropdown-toggle"
-          className={`!bg-white ${
-            user[field] ? "text-black" : "!text-hackathon-placeholder"
-          } w-full !text-left !border-x-0 !border-t-0 !border-b-2 !rounded-none !border-black`}
-        >
-          {user[field] || placeholder}
-        </Dropdown.Toggle>
+        {editable ? (
+          <Dropdown.Toggle
+            id="dropdown-toggle"
+            className={`!bg-white ${
+              user[field] ? "text-black" : "!text-hackathon-placeholder"
+            } w-full !text-left !border-x-0 !border-t-0 !border-b-2 !rounded-none !border-black`}
+          >
+            {user[field] || placeholder}
+          </Dropdown.Toggle>
+        ) : (
+          <div
+            id="dropdown-toggle"
+            className={`placeholder:text-hackathon-placeholder ${
+              user[field] ? "text-black" : "!text-hackathon-placeholder"
+            } w-full pl-3 !border-x-0 !border-t-0 !border-b-2 !rounded-none !border-black`}
+          >
+            {user[field] || placeholder}
+          </div>
+        )}
         {editable && (
           <Dropdown.Menu className="w-full bg-hackathon-green-100 !border-none !rounded-none !p-0 overflow-y-auto max-h-[35vh]">
             {options.map((option, index) => (
