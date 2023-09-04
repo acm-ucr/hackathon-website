c7c325bf517094473d7af009bb4c1514c240617d
Divyank Shah <divyank.shah.2016@gmail.com>
2023-09-03 18:30:57 -0700

    finish tests

--- cypress/component/Tag.cy.jsx
+++ cypress/component/Tag.cy.jsx
@@ -1,13 +1,40 @@
 import Tag from "@/components/Admin/Tag";
 
 describe("Tag", () => {
-  it("Pending", () => {
+  it("Accepted", () => {
     const color = {
       background: "bg-hackathon-tags-green-bg",
       text: "text-hackathon-tags-green-text",
     };
 
-    cy.mount(<Tag color={color} text="Pending" />);
+    const onClick = cy.stub();
+
+    cy.mount(<Tag color={color} text="Accepted" onClick={onClick} />);
+    cy.get(".tag").contains("Accepted");
+    cy.get(".tag").should("have.class", "bg-hackathon-tags-green-bg");
+    cy.get(".tag").should("have.class", "text-hackathon-tags-green-text");
+
+    cy.get(".tag")
+      .click()
+      .then(() => expect(onClick).to.be.called);
+  });
+
+  it("Pending", () => {
+    const color = {
+      background: "bg-hackathon-tags-yellow-bg",
+      text: "text-hackathon-tags-yellow-text",
+    };
+
+    const onClick = cy.stub();
+
+    cy.mount(<Tag color={color} text="Pending" onClick={onClick} />);
+    cy.get(".tag").contains("Pending");
+    cy.get(".tag").should("have.class", "bg-hackathon-tags-yellow-bg");
+    cy.get(".tag").should("have.class", "text-hackathon-tags-yellow-text");
+
+    cy.get(".tag")
+      .click()
+      .then(() => expect(onClick).to.be.called);
   });
 
   it("Rejected", () => {
@@ -16,6 +43,30 @@ describe("Tag", () => {
       text: "text-hackathon-tags-red-text",
     };
 
-    cy.mount(<Tag color={color} text="Rejected" />);
+    const onClick = cy.stub();
+
+    cy.mount(<Tag color={color} text="Rejected" onClick={onClick} />);
+    cy.get(".tag").contains("Rejected");
+    cy.get(".tag").should("have.class", "bg-hackathon-tags-red-bg");
+    cy.get(".tag").should("have.class", "text-hackathon-tags-red-text");
+
+    cy.get(".tag")
+      .click()
+      .then(() => expect(onClick).to.be.called);
+  });
+
+  it("Custom Classes", () => {
+    const color = {
+      background: "bg-hackathon-tags-green-bg",
+      text: "text-hackathon-tags-green-text",
+    };
+
+    cy.mount(
+      <Tag color={color} text="Accepted" classes="border-2 border-black" />
+    );
+    cy.get(".tag").contains("Accepted");
+    cy.get(".tag").should("have.class", "bg-hackathon-tags-green-bg");
+    cy.get(".tag").should("have.class", "text-hackathon-tags-green-text");
+    cy.get(".tag").should("have.class", "border-2 border-black");
   });
 });
