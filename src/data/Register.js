82df7cc169300ada22b42eb866390319a5a34a65
Suhani Chaudhary <suhanichaudhary@Suhanis-MacBook-Pro-2.local>
2023-09-05 17:38:42 -0700

    Change mentor form availability question to checkbox format

--- src/data/Register.js
+++ src/data/Register.js
@@ -59,20 +59,56 @@ export const DietRestrictions = {
   },
 };
 
-export const Availability = [
-  "Friday Morning",
-  "Friday Afternoon",
-  "Friday Evening",
-  "Friday Night",
-  "Saturday Morning",
-  "Saturday Afternoon",
-  "Saturday Evening",
-  "Saturday Night",
-  "Sunday Morning",
-  "Sunday Afternoon",
-  "Sunday Evening",
-  "Sunday Night",
-];
+export const Availability = {
+  fridayMorning: {
+    state: false,
+    text: "Friday Morning",
+  },
+  fridayAfternoon: {
+    state: false,
+    text: "Friday Afternoon",
+  },
+  fridayEvening: {
+    state: false,
+    text: "Friday Evening",
+  },
+  fridayNight: {
+    state: false,
+    text: "Friday Night",
+  },
+  saturdayMorning: {
+    state: false,
+    text: "Saturday Morning",
+  },
+  saturdayAfternoon: {
+    state: false,
+    text: "Saturday Afternoon",
+  },
+  saturdayEvening: {
+    state: false,
+    text: "Saturday Evening",
+  },
+  saturdayNight: {
+    state: false,
+    text: "Saturday Night",
+  },
+  sundayMorning: {
+    state: false,
+    text: "Sunday Morning",
+  },
+  sundayAfternoon: {
+    state: false,
+    text: "Sunday Afternoon",
+  },
+  sundayEvening: {
+    state: false,
+    text: "Sunday Evening",
+  },
+  sundayNight: {
+    state: false,
+    text: "Sunday Night",
+  },
+};
 
 export const Requirements = {
   photography: {
