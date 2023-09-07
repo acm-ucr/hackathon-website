40838873ddea61adab73e47917a712fab3ebbd30
Anjaline Singh <sharansingh@Sharans-MacBook-Air.local>
2023-09-07 13:15:42 -0700

    fixed in tailwind

--- src/components/Forms/Textarea.jsx
+++ src/components/Forms/Textarea.jsx
@@ -32,7 +32,7 @@ const Textarea = ({
       )}
       <textarea
         disabled={editable && !edit}
-        className="pl-3 w-full focus:outline-none placeholder:text-hackathon-gray-200 resize-none"
+        className="pl-3 w-full focus:outline-none placeholder:text-hackathon-gray-200 resize-none h-14"
         type={type}
         name={name}
         placeholder={placeholder}
