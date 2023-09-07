ce7c9de43d09fa3df4118d51be20d66e1642dde7
menthy-wu <wumengxuan0728@gmail.com>
2023-09-06 21:01:45 -0700

    mentor select

---
+++ cypress/e2e/admin/mentors/Select.cy.js
@@ -0,0 +1,33 @@
+import { mentorList } from "../../../../src/data/mock/Mentors";
+
+const five = mentorList.slice(0, 5);
+
+describe("Mentors Select", () => {
+  beforeEach(() => {
+    cy.login("admin");
+    cy.visit("/");
+    cy.wait("@session");
+    cy.visit("/admin/mentors");
+  });
+
+  it("Select All", () => {
+    cy.get('[data-cy="select-all"]').click();
+    mentorList.forEach((mentor) => {
+      cy.get(`[data-cy="${mentor.uid}"]`).should("have.class", "bg-green-100");
+    });
+  });
+
+  it("Select First 5 Entries", () => {
+    five.map((mentor) =>
+      cy.get(`[data-cy="${mentor.uid}"]`).find('[data-cy="checkbox"]').click()
+    );
+    mentorList.forEach((mentor, index) => {
+      if (index < 5)
+        cy.get(`[data-cy="${mentor.uid}"]`).should(
+          "have.class",
+          "bg-green-100"
+        );
+      else cy.get(`[data-cy="${mentor.uid}"]`).should("have.class", "bg-white");
+    });
+  });
+});
