590109acb90552d33dc40d2d5d14656448a47579
SafeDuck <schopra579@gmail.com>
2023-09-06 19:27:54 -0700

    fixed errors

--- cypress/component/Checkbox.cy.jsx
+++ cypress/component/Checkbox.cy.jsx
@@ -1,17 +1,22 @@
 import Checkbox from "@/components/Checkbox";
-import { mount } from "cypress-react-unit-test";
 import { useState } from "react";
 
 describe("Checkbox", () => {
   it("Toggle OFF", () => {
     const toggle = false;
 
-    mount(<Checkbox toggle={toggle} />);
+    cy.mount(<Checkbox toggle={toggle} />);
 
     cy.get('[data-cy="checkbox"]').should("exist");
     cy.get('[data-cy="checkmark"]').should("exist");
-    cy.get('[data-cy="checkmark"]').should("have.class", "text-hackathon-gray");
-    cy.get('[data-cy="checkbox"]').should("have.class", "bg-hackathon-gray");
+    cy.get('[data-cy="checkmark"]').should(
+      "have.class",
+      "text-hackathon-gray-100"
+    );
+    cy.get('[data-cy="checkbox"]').should(
+      "have.class",
+      "bg-hackathon-gray-100"
+    );
   });
 
   it("Toggle ON", () => {
@@ -25,7 +30,7 @@ describe("Checkbox", () => {
       return <Checkbox toggle={toggle} onClick={onClick} />;
     };
 
-    mount(<Parent />);
+    cy.mount(<Parent />);
 
     cy.get('[data-cy="checkbox"]').click();
     cy.get('[data-cy="checkbox"]').should(
