b154378ac953999256602332e10594dd7ed18570
menthy-wu <wumengxuan0728@gmail.com>
2023-09-06 20:55:10 -0700

    mentor searching

---
+++ cypress/e2e/admin/mentor/Search.cy.js
@@ -0,0 +1,22 @@
+import { mentorList } from "../../../../src/data/mock/Mentors";
+
+describe("Mentor Search", () => {
+  beforeEach(() => {
+    cy.login("admin");
+    cy.visit("/");
+    cy.wait("@session");
+    cy.visit("/admin/mentors");
+  });
+
+  it("No Search Results", () => {
+    cy.get('[data-cy="toolbar"]').find("input").type("Meow");
+    cy.get('[data-cy="toolbar"]').find("form").submit();
+    cy.contains("No Mentors Available");
+  });
+
+  it("Search For 1st Entry", () => {
+    cy.get('[data-cy="toolbar"]').find("input").type(mentorList[0].name);
+    cy.get('[data-cy="toolbar"]').find("form").submit();
+    cy.get(`[data-cy="${mentorList[0].uid}"]`).should("exist");
+  });
+});
