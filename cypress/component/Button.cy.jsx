a358bf4401fdf3928bad2ae3be80ffece0b5f042
Divyank Shah <divyank.shah.2016@gmail.com>
2023-09-11 01:38:52 -0700

    fixes

--- cypress/component/Button.cy.jsx
+++ cypress/component/Button.cy.jsx
@@ -10,19 +10,18 @@ describe("Admin Button", () => {
     cy.mount(
       <Button color={color} text={text} onClick={onClick} size={size} />
     );
+
     cy.get('[data-cy="send-button"]').contains("send");
     cy.get('[data-cy="send-button"]').should(
       "have.class",
       "bg-hackathon-green-300"
     );
-    cy.get('[data-cy="send-button"]').should("have.class", "text-white");
-    cy.get('[data-cy="send-button"]').should("have.class", "border-0");
     cy.get('[data-cy="send-button"]').should("have.class", "text-base");
-    cy.get('[data-cy="send-button"]').should("have.class", "hover:opacity-50");
     cy.get('[data-cy="send-button"]')
       .click()
       .then(() => expect(onClick).to.be.called);
   });
+
   it("Check In Button", () => {
     const color = "green";
     const onClick = cy.stub();
@@ -37,8 +36,6 @@ describe("Admin Button", () => {
       "have.class",
       "bg-hackathon-green-300"
     );
-    cy.get('[data-cy="Check In-button"]').should("have.class", "text-white");
-    cy.get('[data-cy="Check In-button"]').should("have.class", "border-0");
     cy.get('[data-cy="Check In-button"]').should("have.class", "text-xl");
     cy.get('[data-cy="Check In-button"]').should(
       "have.class",
