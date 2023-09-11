e0915dd001dabce4c086f08491dc1da98bc35070
fardinzam <fardinzaman51@gmail.com>
2023-09-10 17:23:15 -0700

    added more tests

--- cypress/e2e/admin/participents/Remove.cy.js
+++ cypress/e2e/admin/participents/Remove.cy.js
@@ -1,6 +1,8 @@
 import { participantList } from "../../../../src/data/mock/Participants";
 
 const firstThree = participantList.slice(0, 3);
+const middleThree = participantList.slice(10, 13);
+const lastThree = participantList.slice(17, 20);
 
 describe("Unselected Participants Data", () => {
   beforeEach(() => {
@@ -21,6 +23,36 @@ describe("Unselected Participants Data", () => {
     cy.get(`[data-cy="confirm-button"]`).click();
 
     firstThree.forEach((participant) =>
+      cy.get(`[data-cy="${participant.uid}"]`).should("not.exist")
+    );
+  });
+
+  it("Delete Middle Three Entries", () => {
+    middleThree.forEach((participant) => {
+      cy.get(`[data-cy="${participant.uid}"]`)
+        .find('[data-cy="checkbox"]')
+        .click();
+    });
+
+    cy.get('[data-cy="toolbar"]').find('[data-cy="delete"]').click();
+    cy.get(`[data-cy="confirm-button"]`).click();
+
+    middleThree.forEach((participant) =>
+      cy.get(`[data-cy="${participant.uid}"]`).should("not.exist")
+    );
+  });
+
+  it("Delete Last Three Entries", () => {
+    lastThree.forEach((participant) => {
+      cy.get(`[data-cy="${participant.uid}"]`)
+        .find('[data-cy="checkbox"]')
+        .click();
+    });
+
+    cy.get('[data-cy="toolbar"]').find('[data-cy="delete"]').click();
+    cy.get(`[data-cy="confirm-button"]`).click();
+
+    lastThree.forEach((participant) =>
       cy.get(`[data-cy="${participant.uid}"]`).should("not.exist")
     );
   });
