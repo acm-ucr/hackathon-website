bf1fe2a21860164547332348b3c68175d36aebf5
Divyank Shah <divyank.shah.2016@gmail.com>
2023-09-13 03:40:07 -0700

    remove react import

--- src/components/admin/services/calendar/Events.jsx
+++ src/components/admin/services/calendar/Events.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import moment from "moment";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
