b3478b8456898420789335216f066faced576e79
menthy-wu <wumengxuan0728@gmail.com>
2023-09-16 22:28:20 -0700

    extract icons

---
+++ src/data/admin/Icons.js
@@ -0,0 +1,26 @@
+import { IoIosMail, IoIosRose, IoIosShirt, IoIosSchool } from "react-ICONS/io";
+import { SiGithub, SiDevpost } from "react-ICONS/si";
+import {
+  FaAppleAlt,
+  FaPhoneAlt,
+  FaCrown,
+  FaSchool,
+  FaVenusMars,
+  FaBirthdayCake,
+  FaBook,
+} from "react-icons/fa";
+export const ICONS = {
+  github: <SiGithub className="mr-2" />,
+  devpost: <SiDevpost className="mr-2" />,
+  lead: <IoIosRose className="ml-1 text-hackathon-blue-200 text-lg" />,
+  winner: <FaCrown className="ml-1 text-hackathon-yellow-100 text-lg" />,
+  phone: <FaPhoneAlt className="text-hackathon-blue-200 mr-2" />,
+  email: <IoIosMail className="text-hackathon-blue-200 mr-2 text-lg" />,
+  size: <IoIosShirt className="text-hackathon-blue-200 mr-2 text-lg" />,
+  restriction: <FaAppleAlt className="text-hackathon-blue-200 mr-2" />,
+  age: <FaBirthdayCake className="text-hackathon-blue-200 mr-2 text-lg" />,
+  gender: <FaVenusMars className="text-hackathon-blue-200 mr-2 text-lg" />,
+  grade: <IoIosSchool className="text-hackathon-blue-200 mr-2 text-lg" />,
+  major: <FaBook className="text-hackathon-blue-200 mr-2 text-lg" />,
+  school: <FaSchool className="text-hackathon-blue-200 mr-2 text-lg" />,
+};
