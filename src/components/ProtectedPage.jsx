b3dccc255b05d7a737c020ca4b216e508df0d4ba
menthy-wu <wumengxuan0728@gmail.com>
2023-09-18 01:53:14 -0700

    merge error

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -2,23 +2,17 @@
 import React, { useEffect, useState } from "react";
 import { signIn, useSession } from "next-auth/react";
 import Loading from "@/components/Loading";
-import { usePathname } from "next/navigation";
+import Error from "./Error";
+import Navigation from "./Navigation";
 
 const ProtectedPage = ({ title, children, restrictions }) => {
-  const router = useRouter();
-  const pathName = usePathname();
   const { data: session, status } = useSession();
-  const [unauthorizedError, setUnauthorizedError] = useState("");
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (status === "loading") return;
     if (status !== "authenticated") {
       void signIn("google");
-      return;
-    }
-    if (!session.user.role) {
-      console.log("No Role Assigned");
-      router.push("/");
       return;
     }
     if (
@@ -27,25 +21,24 @@ const ProtectedPage = ({ title, children, restrictions }) => {
       !session.user.role
     ) {
       console.log("Have not register");
-      setUnauthorizedError("You need login to access this page.");
+      setError({
+        code: 401,
+        error: "Unauthenticated User",
+        message: "You need login to access this page",
+      });
       return;
     }
     if (
       status === "authenticated" &&
-      restrictions.includes("committee") &&
-      session.user.role !== "committee"
-    ) {
-      console.log("Dont have permissions");
-      router.push("/");
-      return;
-    }
-    if (
-      status === "authenticated" &&
-      restrictions.includes("admin") &&
-      !session.user.role.includes("admin")
+      restrictions.length > 0 &&
+      !restrictions.includes(session.user.role)
     ) {
       console.log("Dont have admin permissions");
-      setUnauthorizedError("You do not have access this page.");
+      setError({
+        code: 403,
+        error: "Unauthorized",
+        message: "You do not have access this page",
+      });
       return;
     }
   }, [status]);
@@ -53,15 +46,17 @@ const ProtectedPage = ({ title, children, restrictions }) => {
   return (
     <>
       {status === "loading" && <Loading />}
-      {status === "authenticated" && (
-        <div className="w-full flex justify-center h-full">
+      {error && (
+        <Error code={error.code} error={error.error} message={error.message} />
+      )}
+      {status === "authenticated" && !error && (
+        <>
+          <Navigation />
           <title>{title}</title>
-          <div
-            className={`${!pathName.startsWith("/forms") && "w-11/12"} h-full`}
-          >
-            {children}
+          <div className="flex justify-center items-start w-full bg-hackathon-page z-0 h-screen pt-12 lg:pt-0">
+            <div className="w-11/12 h-full">{children}</div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
