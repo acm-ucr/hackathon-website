baa746f2635b04d41b5b883b7166f31e2d1d75c8
Divyank Shah <divyank.shah.2016@gmail.com>
2023-09-19 18:40:11 -0700

    add release comp

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
 import LoginButton from "@/components/LoginButton";
+import Release from "@/components/Release";
+import { releases } from "@/data/Release";
+
 const Home = () => {
-  return <LoginButton />;
+  return (
+    <>
+      <Release release={releases.comingsoon}>Coming Soon Compomnent</Release>
+      <LoginButton />
+    </>
+  );
 };
 
 export default Home;
