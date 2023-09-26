1f3e4c90bbba7da2f5df99ad730efbb9ae7ed3e1
Byron Bhuiyan <rafidb8@gmail.com>
2023-09-25 23:51:06 -0700

    Fixed component test

--- cypress/component/admin/SortIcon.cy.jsx
+++ cypress/component/admin/SortIcon.cy.jsx
@@ -1,44 +1,92 @@
-import SortIcon from "../../../src/components/admin/dashboards/SortIcon.jsx";
+import React, { useState } from "react";
+import SortIcon from "@/components/admin/dashboards/SortIcon";
 
 describe("SortIcon", () => {
-  let setHeadersStub;
-  let setObjectsStub;
-  let setCurrentSortStub;
-  const mockData = {
-    index: 0,
-    name: "test",
-    headers: [{ text: "test", sort: "off" }],
-    objects: [{ test: "A" }, { test: "B" }],
-    currentSort: "test",
-  };
+  it("Exists", () => {
+    const Parent = () => {
+      const [headers, setHeaders] = useState([{ text: "name", sort: "off" }]);
+      const [currentSort, setCurrentSort] = useState(null);
+      const setObjects = () => {};
 
-  beforeEach(() => {
-    setHeadersStub = cy.stub();
-    setObjectsStub = cy.stub();
-    setCurrentSortStub = cy.stub();
-  });
+      return (
+        <SortIcon
+          index={0}
+          name="name"
+          headers={headers}
+          setHeaders={setHeaders}
+          setObjects={setObjects}
+          setCurrentSort={setCurrentSort}
+          currentSort={currentSort}
+        />
+      );
+    };
 
-  const mountComponent = (overrides = {}) => {
-    cy.mount(
-      <SortIcon
-        {...mockData}
-        setHeaders={setHeadersStub}
-        setObjects={setObjectsStub}
-        setCurrentSort={setCurrentSortStub}
-        {...overrides}
-      />
+    cy.mount(<Parent />);
+
+    cy.get('[data-cy="name-sort-up"]').should("exist");
+    cy.get('[data-cy="name-sort-down"]').should("exist");
+    cy.get('[data-cy="name-sort-up"]').should("have.class", "hover:opacity-50");
+    cy.get('[data-cy="name-sort-down"]').should(
+      "have.class",
+      "hover:opacity-50"
     );
-  };
-
-  it("Up button highlights when clicked", () => {
-    mountComponent();
-    cy.get("#up").click();
-    cy.get("#up").should("have.class", "text-[8px]");
   });
 
-  it("Down button highlights when clicked", () => {
-    mountComponent();
-    cy.get("#down").click();
-    cy.get("#down").should("have.class", "text-[8px]");
+  it("Up", () => {
+    const Parent = () => {
+      const [headers, setHeaders] = useState([{ text: "name", sort: "off" }]);
+      const [currentSort, setCurrentSort] = useState(null);
+      const setObjects = () => {};
+
+      return (
+        <SortIcon
+          index={0}
+          name="name"
+          headers={headers}
+          objects={[]}
+          setHeaders={setHeaders}
+          setObjects={setObjects}
+          setCurrentSort={setCurrentSort}
+          currentSort={currentSort}
+        />
+      );
+    };
+
+    cy.mount(<Parent />);
+
+    cy.get('[data-cy="name-sort-up"]').click();
+    cy.get('[data-cy="name-sort-up"]').should(
+      "have.class",
+      "text-hackathon-blue-100"
+    );
+  });
+
+  it("Down", () => {
+    const Parent = () => {
+      const [headers, setHeaders] = useState([{ text: "name", sort: "off" }]);
+      const [currentSort, setCurrentSort] = useState(null);
+      const setObjects = () => {};
+
+      return (
+        <SortIcon
+          index={0}
+          name="name"
+          headers={headers}
+          objects={[]}
+          setHeaders={setHeaders}
+          setObjects={setObjects}
+          setCurrentSort={setCurrentSort}
+          currentSort={currentSort}
+        />
+      );
+    };
+
+    cy.mount(<Parent />);
+
+    cy.get('[data-cy="name-sort-down"]').click();
+    cy.get('[data-cy="name-sort-down"]').should(
+      "have.class",
+      "text-hackathon-blue-100"
+    );
   });
 });
