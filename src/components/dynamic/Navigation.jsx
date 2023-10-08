e1b1b6f8d9e4026d3ea53310bc743cc3853af81c
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-10-07 23:48:18 -0700

    added logo

--- src/components/dynamic/Navigation.jsx
+++ src/components/dynamic/Navigation.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import LOGO from "../../../public/LOGO.png";
+import LOGO from "../../../public/UCRLOGO.png";
 import Image from "next/image";
 import Link from "next/link";
 import { ADMIN, USER } from "@/data/dynamic/Navigation";
