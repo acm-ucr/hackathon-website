6e0a59633840c470a28c291c0168e77c4d61b206
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-10-07 19:55:38 -0700

    resized button

--- src/components/dynamic/admin/Button.jsx
+++ src/components/dynamic/admin/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ color, text, onClick, size, disabled = false }) => {
   return (
     <button
       data-cy={`${text}-button`}
-      className={`${COLORS[color].bg} ${COLORS[color].text} ${SIZES[size]} ${COLORS[color].border} py-1 disabled:opacity-50 hover:opacity-50 font-bold rounded-lg px-4`}
+      className={`${COLORS[color].bg} ${COLORS[color].text} ${SIZES[size]} ${COLORS[color].border} mt-1 mb-1 py-1 disabled:opacity-50 hover:opacity-50 font-bold rounded-lg px-4`}
       onClick={onClick}
       disabled={disabled}
     >
