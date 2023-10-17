646fe78c3907467c5347cd554fb648d865dd2e40
Andrew Khadder <andrewkh15@gmail.com>
2023-10-17 10:56:14 -0700

    changed manifest.js to arrow function

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,10 +1,12 @@
 import { MANIFEST } from "@/data/Config";
 
-export default function manifest() {
+const manifest = () => {
   return {
     name: MANIFEST.name,
     short_name: MANIFEST.short_name,
     description: MANIFEST.description,
     start_url: MANIFEST.start_url,
   };
-}
+};
+
+export default manifest;
