4495526e27b6a2ec634d21cdb258ca7a699ffa3a
Andrew Khadder <andrewkh15@gmail.com>
2023-10-23 15:21:09 -0700

    Revert "removed 'use client'"
    This reverts commit efd3d8fce33c4494dff017304446c7d3c86fabc8.

--- src/components/dynamic/admin/dashboards/Mentors.jsx
+++ src/components/dynamic/admin/dashboards/Mentors.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Dashboard from "../Dashboard.jsx";
 import {
   FILTERS,
--- src/components/dynamic/admin/dashboards/Teams.jsx
+++ src/components/dynamic/admin/dashboards/Teams.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Dashboard from "../Dashboard.jsx";
 import { FILTERS, HEADERS, TAGS, STATUSES } from "@/data/dynamic/admin/Teams";
 
