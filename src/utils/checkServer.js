d61f30decd3aa392da47fcc6ac403670e4c13f83
Val <vwong031@ucr.edu>
2023-10-31 21:29:54 -0700

    changed to for each loop & differentiated the files?

--- src/utils/checkServer.js
+++ src/utils/checkServer.js
@@ -1,17 +1,17 @@
 export const checkServer = (data) => {
   const errors = [];
 
-  for (const key in data.textInputs) {
-    if (data.textInputs[key].length > 100) {
+  data.textInputs.forEach((textInput, key) => {
+    if (textInput.length > 100) {
       errors.push("Text input ${key} exceeds 100 characters.");
     }
-  }
+  });
 
-  for (const key in data.base64Files) {
-    if (data.base64Files[key].length > 200 * 1024 * 1024) {
+  data.base64Files.forEach((file, key) => {
+    if (file.length > 200 * 1024 * 1024) {
       errors.push("File ${key} exceeds 200MB.");
     }
-  }
+  });
 
   if (errors.length > 0) {
     return { valid: false, errors };
