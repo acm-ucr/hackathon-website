44bc7751f2dfb7838d099d282516e6d05fad03d5
Divyank Shah <divyank.shah.2016@gmail.com>
2023-11-05 16:44:50 -0800

    merge 2 calendars

--- src/components/dynamic/admin/services/calendar/Events.jsx
+++ src/components/dynamic/admin/services/calendar/Events.jsx
@@ -29,24 +29,33 @@ const CalendarEvents = () => {
   };
 
   useEffect(() => {
-    axios
-      .get(
-        `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_EMAIL}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`
-      )
-      .then((response) => {
-        const items = response.data.items.map((item) => {
-          item.start = new Date(item.start.dateTime);
-          item.end = new Date(item.end.dateTime);
-          item.color =
-            LABELS[
-              item.description.split("\n")[1].split(": ")[1].toLowerCase()
-            ].background;
-          item.hidden = false;
+    const hackathon = axios.get(
+      `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`
+    );
 
-          return item;
-        });
-        setEvents(items);
+    const leads = axios.get(
+      `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_LEADS}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`
+    );
+
+    Promise.all([hackathon, leads]).then(([hackathonData, leadsData]) => {
+      const hackathon = hackathonData.data.items;
+      const leads = leadsData.data.items;
+
+      const rawEvents = [...hackathon, ...leads];
+
+      rawEvents.forEach((item) => {
+        item.start = new Date(item.start.dateTime);
+        item.end = new Date(item.end.dateTime);
+        item.color =
+          LABELS[
+            item.description.split("\n")[1].split(": ")[1].toLowerCase()
+          ].background;
+        item.hidden = false;
       });
+
+      setEvents(rawEvents);
+    });
+
     document.addEventListener("keydown", handleShortcuts);
     return () => document.removeEventListener("keydown", handleShortcuts);
   }, []);
@@ -59,6 +68,7 @@ const CalendarEvents = () => {
           date={date}
           view={view}
           className="py-4"
+          step={15}
           events={events.filter((event) => !event.hidden)}
           localizer={mLocalizer}
           defaultView="month"
@@ -66,7 +76,7 @@ const CalendarEvents = () => {
           onNavigate={(newDate) => setDate(newDate)}
           onView={(newView) => setView(newView)}
           components={{
-            event: Event,
+            event: (props) => <Event {...props} view={view} />,
             toolbar: (props) => (
               <Toolbar {...props} events={events} setEvents={setEvents} />
             ),
