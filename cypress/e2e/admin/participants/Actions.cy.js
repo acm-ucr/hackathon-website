f56905bc352807c0af0779c48d2714703ac04092
fardinzam <fardinzaman51@gmail.com>
2023-11-26 20:13:47 -0800

    text fix

--- cypress/e2e/admin/participants/Actions.cy.js
+++ cypress/e2e/admin/participants/Actions.cy.js
@@ -19,7 +19,7 @@ describe("Participants Actions", () => {
         .click()
     );
     cy.action({
-      tag: "accept",
+      tag: "accepted",
       page: "participants",
     });
 
@@ -39,7 +39,7 @@ describe("Participants Actions", () => {
         .click()
     );
     cy.action({
-      tag: "reject",
+      tag: "rejected",
       page: "participants",
     });
 
