0891225a3a1b220e279538e33a40866b14628ca0
seanquiambao <slquiambao03@gmail.com>
2023-11-28 11:46:58 -0800

    fixed calendar filters

--- src/components/dynamic/admin/services/calendar/Toolbar.jsx
+++ src/components/dynamic/admin/services/calendar/Toolbar.jsx
@@ -24,8 +24,12 @@ const CustomToolbar = ({
       setEvents(
         events.map((event) => {
           if (
-            event.description.split("\n")[1].split(": ")[1].toLowerCase() ===
-            value
+            event.description
+              .split("\n")[0]
+              .split("#")
+              .map((item) => item.trim())
+              .filter((item) => item !== "")[0]
+              .toLowerCase() === value
           ) {
             event.hidden = false;
           } else {
