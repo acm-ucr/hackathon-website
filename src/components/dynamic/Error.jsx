1105ebca2dc0543c2b931b3e06bebce5ddd5f15a
Val <vwong031@ucr.edu>
2023-12-06 19:42:00 -0800

    extend custom error

--- src/components/dynamic/Error.jsx
+++ src/components/dynamic/Error.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Error = ({ code, error, message }) => {
+const Error = ({ code, error, message, dev }) => {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center fixed">
       <p className="text-center text-6xl font-extrabold text-hackathon-blue-100 m-0">
@@ -11,6 +11,13 @@ const Error = ({ code, error, message }) => {
       </p>
       <p className="text-center text-sm md:text-lg  text-hackathon-blue-200 m-0">
         {message}
+      </p>
+      <p>
+        {dev && (
+          <p className="text-center text-lg md:text-2xl font-bold text-black m-0">
+            Developer Notes: {dev}
+          </p>
+        )}
       </p>
     </div>
   );
