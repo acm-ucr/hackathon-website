a5384e50f9aae8d560a04816ca5469c6b0326a88
fardinzam <fardinzaman51@gmail.com>
2023-12-27 18:21:03 -0800

    changed layout

--- src/app/admin/[adminPageName]/page.js
+++ src/app/admin/[adminPageName]/page.js
@@ -1,5 +1,4 @@
 import ProtectedPage from "@/components/dynamic/ProtectedPage";
-import NotFoundError from "@/app/not-found";
 import Admins from "@/components/dynamic/admin/dashboards/Admins";
 import Events from "@/components/dynamic/admin/services/calendar/Events";
 import CheckIn from "@/components/dynamic/admin/services/checkin/CheckIn";
@@ -17,36 +16,45 @@ import Statistics from "@/components/dynamic/admin/services/statistics/Statistic
 import Teams from "@/components/dynamic/admin/dashboards/Teams";
 import Volunteers from "@/components/dynamic/admin/dashboards/Volunteers";
 
-const adminPagesSpecs = {
-  admins: <Admins />,
-  calendar: <Events />,
-  checkin: <CheckIn />,
-  committees: <Committees />,
-  feedback: <Feedback />,
-  interests: <Interests />,
-  judges: <Judges />,
-  judging: <Judging />,
-  mentors: <Mentors />,
-  messenger: <Messenger />,
-  participants: <Participants />,
-  prizes: <Prizes />,
-  sponsors: <Sponsors />,
-  statistics: <Statistics />,
-  teams: <Teams />,
-  volunteers: <Volunteers />,
-};
+const Page = ({ params }) => {
+  const components = {
+    admins: <Admins />,
+    calendar: <Events />,
+    checkin: <CheckIn />,
+    committees: <Committees />,
+    feedback: <Feedback />,
+    interests: <Interests />,
+    judges: <Judges />,
+    judging: <Judging />,
+    mentors: <Mentors />,
+    messenger: <Messenger />,
+    participants: <Participants />,
+    prizes: <Prizes />,
+    sponsors: <Sponsors />,
+    statistics: <Statistics />,
+    teams: <Teams />,
+    volunteers: <Volunteers />,
+  };
 
-export default function Page({ params }) {
   const pageName = params.adminPageName;
   const displayName = pageName.charAt(0).toUpperCase() + pageName.slice(1);
-  return adminPagesSpecs[pageName] != undefined ? (
-    <ProtectedPage
-      title={`Admin | ${displayName}`}
-      restrictions={{ admins: 1, committees: 1 }}
-    >
-      {adminPagesSpecs[pageName]}
-    </ProtectedPage>
-  ) : (
-    <NotFoundError />
-  );
-}
+
+  if (components[pageName] != undefined) {
+    return (
+      <ProtectedPage
+        title={`Admin | ${displayName}`}
+        restrictions={{ admins: 1, committees: 1 }}
+      >
+        {components[pageName]}
+      </ProtectedPage>
+    );
+  } else {
+    throw new Fault(
+      404,
+      "Page Not Found",
+      "The page you are looking for does not seem to exist"
+    );
+  }
+};
+
+export default Page;
