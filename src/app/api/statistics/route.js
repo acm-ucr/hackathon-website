00f785c168ba62a2b1844bd7fefcf3d1b0e41455
theo <theobfernandez@gmail.com>
2024-01-05 18:31:33 -0800

    remove comments

--- src/app/api/statistics/route.js
+++ src/app/api/statistics/route.js
@@ -3,41 +3,6 @@ import { db } from "../../../utils/firebase";
 import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 import { authenticate } from "@/utils/auth";
 
-// used for initial setup, in case becomes innacurate
-// async function syncStatsWithDatabase() {
-//   await updateRoleCounts("participants");
-// 	await updateRoleCounts("volunteers");
-// 	await updateRoleCounts("judges");
-// 	await updateRoleCounts("mentors");
-// 	await updateRoleCounts("committees");
-// 	await updateRoleCounts("sponsors");
-// 	await updateRoleCounts("admins");
-//   await updateRoleCounts("teams");
-// }
-// const getRoleCount = async (role, value) => {
-//   return (
-//     await getDocs(
-//       query(
-//         collection(db, "users"),
-//         where(`roles.${role}`, "==", value)
-//       )
-//     )
-//   ).size;
-// };
-// async function updateRoleCounts(role) {
-//   const [roleMinusOneCount, roleZeroCount, roleOneCount] = await Promise.all([
-//     getRoleCount(role, -1),
-//     getRoleCount(role, 0),
-//     getRoleCount(role, 1),
-//   ]);
-
-//   await updateDoc(doc(db, "statistics", "statistics"), {
-//     [`${role}.-1`]: roleMinusOneCount,
-//     [`${role}.0`]: roleZeroCount,
-//     [`${role}.1`]: roleOneCount,
-//   });
-// }
-
 export async function GET() {
   const res = NextResponse;
   const { auth } = await authenticate({
@@ -52,7 +17,6 @@ export async function GET() {
   }
 
   try {
-    // await syncStatsWithDatabase();
     const docStats = await getDoc(doc(db, "statistics", "statistics"));
 
     const participants = docStats.data().participants["1"];
