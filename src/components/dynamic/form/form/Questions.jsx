3c22abe08ef6f044f91e46efc3d8e4849894fb63
Andrew Khadder <andrewkh15@gmail.com>
2024-01-17 11:29:25 -0800

    changed astrisk to red

--- src/components/dynamic/form/form/Questions.jsx
+++ src/components/dynamic/form/form/Questions.jsx
@@ -62,7 +62,7 @@ const Questions = ({
                 {index === field.texts.length - 1 && (
                   <p>
                     Fields with
-                    <span className="text-hackathon-green-300"> * </span>
+                    <span className="text-hackathon-red-500"> * </span>
                     are required.
                   </p>
                 )}
