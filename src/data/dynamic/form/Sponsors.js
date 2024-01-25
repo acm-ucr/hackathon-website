303e3ddd7236d997411d78d227416a78381929c0
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-01-25 01:15:12 -0800

    added to rest of forms

--- src/data/dynamic/form/Sponsors.js
+++ src/data/dynamic/form/Sponsors.js
@@ -1,3 +1,5 @@
+import { METADATA } from "@/data/Config";
+
 export const TIERS = {
   tier1: "Bronze",
   tier2: "Silver",
@@ -12,8 +14,8 @@ export const FIELDS = {
     input: "description",
     width: 12,
     texts: [
-      "Welcome to HACKATHON_NAME. Thank you for being on the Sponsorship team, we appreciate your efforts to help support HACKATHON_NAME. HACKATHON_NAME is a DESCRIPTION hackathon spanning HACKATHON_LENGTH on HACKATHON_DATE. ",
-      "By sponsoring HACKATHON_NAME, you help advocate for SPONSOR_REASONS.",
+      `Welcome to ${METADATA.name}. Thank you for being on the Sponsorship team, we appreciate your efforts to help support ${METADATA.name}. ${METADATA.name} is a ${METADATA.description} hackathon spanning ${METADATA.length} on ${METADATA.date}.`,
+      `By sponsoring ${METADATA.name}, you help advocate for SPONSOR_REASONS.`,
       "Sponsorship members are not required to stay the full duration of the event, but are encouraged to checkout the various events, workshops, and opportunities that are available.",
     ],
   },
