40f848bee0c528c2bd25c52df02ddb3f0f839a08
Fuyuki Malahom <fuyuki325@gmail.com>
2024-02-06 20:45:54 -0800

    Toaster with Tailwind

--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -1,30 +1,38 @@
 import { toast } from "react-hot-toast";
+import { Checkmark } from "react-checkmark";
+import { MdError } from "react-icons/md";
 
 const toaster = (message, type) => {
   switch (type) {
     case "success":
-      toast.success(message, {
-        style: {
-          backgroundColor: "#34D399",
-          color: "#ffffff",
-        },
-      });
+      toast.custom(
+        <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
+          <Checkmark size="medium" />
+          <div className="inline ps-4 text-sm font-normal text-black">
+            {message}
+          </div>
+        </div>
+      );
       break;
     case "error":
-      toast.error(message, {
-        style: {
-          background: "#FF2800",
-          color: "#ffffff",
-        },
-      });
+      toast.custom(
+        <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
+          <MdError className="animate-pulse text-red-500" size="35px" />
+          <div className="inline ps-4 text-sm font-normal text-black">
+            {message}
+          </div>
+        </div>
+      );
       break;
     default:
-      toast.error(message, {
-        style: {
-          background: "#FF2800",
-          color: "#ffffff",
-        },
-      });
+      toast.custom(
+        <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
+          <MdError className="animate-pulse text-red-500" size="35px" />
+          <div className="inline ps-4 text-sm font-normal text-black">
+            {message}
+          </div>
+        </div>
+      );
   }
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/data/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
