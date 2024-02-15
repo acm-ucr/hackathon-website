0fec9d7ef77d14ac0343f5f88d40b0033bba41e7
Arkes123 <jonathan.darius2015@gmail.com>
2024-02-15 14:32:36 -0800

    fixed pulse animation in toaster.js

--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -7,7 +7,7 @@ const toaster = (message, type) => {
     case "success":
       toast.custom(
         <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
-          <FaCheckCircle className="animate-pulse text-green-500" />
+          <FaCheckCircle className=" text-green-500" />
           <div className="inline ps-4 text-sm font-normal text-black">
             {message}
           </div>
@@ -17,7 +17,7 @@ const toaster = (message, type) => {
     case "error":
       toast.custom(
         <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
-          <MdError className="animate-pulse text-red-500" size="35px" />
+          <MdError className=" text-red-500" size="35px" />
           <div className="inline ps-4 text-sm font-normal text-black">
             {message}
           </div>
@@ -27,7 +27,7 @@ const toaster = (message, type) => {
     default:
       toast.custom(
         <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
-          <MdError className="animate-pulse text-red-500" size="35px" />
+          <MdError className=" text-red-500" size="35px" />
           <div className="inline ps-4 text-sm font-normal text-black">
             {message}
           </div>
