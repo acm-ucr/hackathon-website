e4dadf16a20aeb5a90e8c7d05de3b3e744a3f14e
Arkes123 <jonathan.darius2015@gmail.com>
2024-02-16 23:01:30 -0800

    Replaced original toaster with new toaster that uses mapping to assigend types

--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -3,29 +3,14 @@ import { MdError } from "react-icons/md";
 import { FaCheckCircle } from "react-icons/fa";
 
 const toaster = (message, type) => {
-  switch (type) {
-    case "success":
-      toast.custom(consolidateToaster(message, type));
-      break;
-    case "error":
-      toast.custom(consolidateToaster(message, type));
-      break;
-    default:
-      toast.custom(consolidateToaster(message, type));
-      break;
-  }
-};
+  const iconMapping = {
+    success: <FaCheckCircle className="text-green-500" size="35px" />,
+    error: <MdError className="text-red-500" size="35px" />,
+  };
 
-const consolidateToaster = (message, type) => {
-  const icon =
-    type === "success" ? (
-      <FaCheckCircle className="text-green-500" size="35px" />
-    ) : (
-      <MdError className="text-red-500" size="35px" />
-    );
-  return (
+  return toast.custom(
     <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
-      {icon}
+      {iconMapping[type] || <MdError className="text-red-500" size="35px" />}
       <div className="inline ps-4 text-sm font-normal text-black">
         {message}
       </div>
