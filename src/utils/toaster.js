78ae89e59b38f53d407500681cff7f397900f556
Arkes123 <jonathan.darius2015@gmail.com>
2024-02-16 22:18:58 -0800

    Consolidated toaster into singular component

--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -5,35 +5,32 @@ import { FaCheckCircle } from "react-icons/fa";
 const toaster = (message, type) => {
   switch (type) {
     case "success":
-      toast.custom(
-        <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
-          <FaCheckCircle className=" text-green-500" />
-          <div className="inline ps-4 text-sm font-normal text-black">
-            {message}
-          </div>
-        </div>
-      );
+      toast.custom(consolidateToaster(message, type));
       break;
     case "error":
-      toast.custom(
-        <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
-          <MdError className=" text-red-500" size="35px" />
-          <div className="inline ps-4 text-sm font-normal text-black">
-            {message}
-          </div>
-        </div>
-      );
+      toast.custom(consolidateToaster(message, type));
       break;
     default:
-      toast.custom(
-        <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
-          <MdError className=" text-red-500" size="35px" />
-          <div className="inline ps-4 text-sm font-normal text-black">
-            {message}
-          </div>
-        </div>
-      );
+      toast.custom(consolidateToaster(message, type));
+      break;
   }
+};
+
+const consolidateToaster = (message, type) => {
+  const icon =
+    type === "success" ? (
+      <FaCheckCircle className="text-green-500" size="35px" />
+    ) : (
+      <MdError className="text-red-500" size="35px" />
+    );
+  return (
+    <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
+      {icon}
+      <div className="inline ps-4 text-sm font-normal text-black">
+        {message}
+      </div>
+    </div>
+  );
 };
 
 export default toaster;
