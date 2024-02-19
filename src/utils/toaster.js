85639f3fa0b513edd1ef8387ddc74b8be603c0b9
Arkes123 <jonathan.darius2015@gmail.com>
2024-02-19 14:05:29 -0800

    fixed to no default state and changed iconMapping -> icons

--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -3,14 +3,14 @@ import { MdError } from "react-icons/md";
 import { FaCheckCircle } from "react-icons/fa";
 
 const toaster = (message, type) => {
-  const iconMapping = {
+  const icons = {
     success: <FaCheckCircle className="text-green-500" size="35px" />,
     error: <MdError className="text-red-500" size="35px" />,
   };
 
   return toast.custom(
     <div className="flex items-center justify-center bg-white pl-2 pr-5 md:pr-6 py-4 w-4/12 md:w-2/12 shadow-md rounded">
-      {iconMapping[type] || <MdError className="text-red-500" size="35px" />}
+      {icons[type]}
       <div className="inline ps-4 text-sm font-normal text-black">
         {message}
       </div>
