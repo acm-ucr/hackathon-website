7bd3702aad37244bbcafe7a5a2b290fb9415331e
Arkes123 <jonathan.darius2015@gmail.com>
2024-02-25 23:34:52 -0800

    fixed week view event styles

--- src/components/dynamic/admin/services/calendar/Events.jsx
+++ src/components/dynamic/admin/services/calendar/Events.jsx
@@ -78,6 +78,10 @@ const CalendarEvents = () => {
           localizer={mLocalizer}
           defaultView="month"
           views={["month", "week"]}
+          formats={{
+            eventTimeRangeFormat: ({ start }) =>
+              mLocalizer.format(start, "hh:mm A\n"),
+          }}
           onNavigate={(newDate) => setDate(newDate)}
           onView={(newView) => setView(newView)}
           components={{
@@ -87,7 +91,12 @@ const CalendarEvents = () => {
             ),
           }}
           eventPropGetter={(event) => {
-            return { className: event.color };
+            return {
+              style: {
+                border: "0px",
+              },
+              className: event.color,
+            };
           }}
           dayPropGetter={(event) => {
             const bg =
