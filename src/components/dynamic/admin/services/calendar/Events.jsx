82af7d999ef81f60a5ab57b4163119f5dfd961ee
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-02-28 19:00:09 -0800

    rsc changes

--- src/components/dynamic/admin/services/calendar/Events.jsx
+++ src/components/dynamic/admin/services/calendar/Events.jsx
@@ -1,81 +1,80 @@
 "use client";
-import { useEffect, useState } from "react";
-import moment from "moment";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+// import { useEffect, useState } from "react";
+// import moment from "moment";
+// import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import Toolbar from "./Toolbar";
-import Event from "./Event";
-import Modal from "./Modal";
+// import Toolbar from "./Toolbar";
+// import Event from "./Event";
+// import Modal from "./Modal";
 import { LABELS } from "@/data/dynamic/admin/Calendar";
 import { api } from "@/utils/api";
-const mLocalizer = momentLocalizer(moment);
+import CalendarWrapper from "./EventsWrapper";
+// import { event } from "cypress/types/jquery";
+// import { event } from "cypress/types/jquery";
+// const mLocalizer = momentLocalizer(moment);
 
-const CalendarEvents = () => {
-  const [events, setEvents] = useState([]);
-  const [event, setEvent] = useState();
-  const [date, setDate] = useState(new Date());
-  const [view, setView] = useState("month");
+const CalendarEvents = async () => {
+  // const [events, setEvents] = useState([]);
+  // const [event, setEvent] = useState();
+  // const [date, setDate] = useState(new Date());
+  // const [view, setView] = useState("month");
 
-  const handleShortcuts = (e) => {
-    switch (e.key) {
-      case "m":
-        setView("month");
-        break;
-      case "w":
-        setView("week");
-        break;
-    }
-  };
+  // const handleShortcuts = (e) => {
+  //   switch (e.key) {
+  //     case "m":
+  //       setView("month");
+  //       break;
+  //     case "w":
+  //       setView("week");
+  //       break;
+  //   }
+  // };
 
-  useEffect(() => {
-    const min = new Date(
-      new Date().getTime() - 20 * 7 * 24 * 60 * 60 * 1000
-    ).toISOString();
+  const min = new Date(
+    new Date().getTime() - 20 * 7 * 24 * 60 * 60 * 1000
+  ).toISOString();
 
-    const max = new Date(
-      new Date().getTime() + 20 * 7 * 24 * 60 * 60 * 1000
-    ).toISOString();
+  const max = new Date(
+    new Date().getTime() + 20 * 7 * 24 * 60 * 60 * 1000
+  ).toISOString();
 
-    const hackathon = api({
-      method: "GET",
-      url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime&timeMin=${min}&timeMax=${max}`,
-    });
+  const hackathon = await api({
+    method: "GET",
+    url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime&timeMin=${min}&timeMax=${max}`,
+  });
 
-    const leads = api({
-      method: "GET",
-      url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_LEADS}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime&timeMin=${min}&timeMax=${max}`,
-    });
+  const leads = await api({
+    method: "GET",
+    url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_LEADS}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime&timeMin=${min}&timeMax=${max}`,
+  });
 
-    Promise.all([hackathon, leads]).then(([hackathonData, leadsData]) => {
-      const hackathonItems = hackathonData.items;
-      const leadsItems = leadsData.items;
+  const hackathonItems = hackathon.items;
+  const leadsItems = leads.items;
 
-      const rawEvents = [...hackathonItems, ...leadsItems];
+  const rawEvents = [...hackathonItems, ...leadsItems];
 
-      rawEvents.forEach((item) => {
-        item.start = new Date(item.start.dateTime);
-        item.end = new Date(item.end.dateTime);
-        const [category, assignee] = item.description
-          .split("\n")[0]
-          .split("#")
-          .map((item) => item.trim())
-          .filter((item) => item !== "");
-        item.category = category;
-        item.color = LABELS[item.category].background;
-        item.assignee = assignee;
-        item.hidden = false;
-      });
+  rawEvents.forEach((item) => {
+    item.start = new Date(item.start.dateTime);
+    item.end = new Date(item.end.dateTime);
+    const [category, assignee] = item.description
+      .split("\n")[0]
+      .split("#")
+      .map((item) => item.trim())
+      .filter((item) => item !== "");
+    item.category = category;
+    item.color = LABELS[item.category].background;
+    item.assignee = assignee;
+    item.hidden = false;
+  });
 
-      setEvents(rawEvents);
-    });
-
-    document.addEventListener("keydown", handleShortcuts);
-    return () => document.removeEventListener("keydown", handleShortcuts);
-  }, []);
+  //   document.addEventListener("keydown", handleShortcuts);
+  //   return () => document.removeEventListener("keydown", handleShortcuts);
+  // }, []);
 
   return (
     <div className="relative h-screen">
-      {event && <Modal event={event} setEvent={setEvent} />}
+      <CalendarWrapper events={rawEvents} data={rawEvents} />
+      {/* {event && <Modal event={event} setEvent={setEvent} />}
       {events && (
         <Calendar
           date={date}
@@ -130,7 +129,7 @@ const CalendarEvents = () => {
           onSelectEvent={(event) => setEvent(event)}
           onDrillDown={() => setView("week")}
         />
-      )}
+      )} */}
     </div>
   );
 };
