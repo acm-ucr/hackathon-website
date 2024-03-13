7fcc1adfbc4f78f395ef9b39b731f6a046eeea06
RiyanaD <riyana.duttaa@gmail.com>
2024-03-13 12:38:09 -0700

    fixed participant terms and condition

--- src/data/dynamic/form/Interest.js
+++ src/data/dynamic/form/Interest.js
@@ -34,6 +34,7 @@ export const FIELDS = {
     input: "terms",
     width: 12,
     field: "requirements",
+    required: true,
     options: [
       "I have read the MLH code of conduct and agree to the terms and conditions listed",
       "I consent to photographs being taken and being used for future marketing purposes",
@@ -41,9 +42,7 @@ export const FIELDS = {
       "I consent to following the provided guidelines and rules instructed by the organizing team",
       "I understand that failure to comply with guidelines or creating an unsafe space will result in my removal from the event",
       "I understand this is an in person event taking place in UCR and I must attend in person in order to participate",
-      "I understand that I will be given access to private data and malicious intents and actions will be reported immediately.",
     ],
-    required: true,
   },
 };
 
