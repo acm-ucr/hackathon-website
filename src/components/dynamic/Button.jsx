0750fee19749b54edd1357c37650695c6089ee3e
javi <javi@Javis-MacBook-Pro-3.local>
2024-03-14 01:29:08 -0700

    Reduce Button Radius from xl to md

--- src/components/dynamic/Button.jsx
+++ src/components/dynamic/Button.jsx
@@ -9,7 +9,7 @@ const Button = ({ onClick, text, loading, color, size }) => {
         loading ? COLORS["gray"].text : COLORS[color].text
       } ${SIZES[size]} ${
         COLORS[color].border
-      } py-1 hover:opacity-50 font-bold px-2 rounded-xl mt-3`}
+      } py-1 hover:opacity-50 font-bold px-4 rounded-md mt-3`}
       onClick={onClick}
     >
       {loading ? "Loading..." : text}
