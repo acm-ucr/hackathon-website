59a4c1c5e07bf378531f8ca4be4a10a4ca24a04f
Atharva <147217989+atharvanev@users.noreply.github.com>
2024-03-30 02:24:56 -0700

    removed exta div

--- src/components/dynamic/user/Rooms.jsx
+++ src/components/dynamic/user/Rooms.jsx
@@ -1,17 +1,15 @@
 const Rooms = ({ rooms }) => {
   return (
-    <div>
-      <div className="font-bold p-3 rounded-lg mt-3 mb-3 bg-white shadow-xl">
-        <div className="p-2">💻 Hackrooms</div>
-        {rooms.map((room, index) => (
-          <div
-            key={index}
-            className="inline-block p-2 rounded-lg text-hackathon-tags-green-text bg-hackathon-green-100 mb-3 shadow mr-3"
-          >
-            {room}
-          </div>
-        ))}
-      </div>
+    <div className="font-bold p-3 rounded-lg mt-3 mb-3 bg-white shadow-xl">
+      <div className="p-2">💻 Hackrooms</div>
+      {rooms.map((room, index) => (
+        <div
+          key={index}
+          className="inline-block p-2 rounded-lg text-hackathon-tags-green-text bg-hackathon-green-100 mb-3 shadow mr-3"
+        >
+          {room}
+        </div>
+      ))}
     </div>
   );
 };
