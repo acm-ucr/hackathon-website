dd506ec112fa4838e204ab018c6878e70e952579
jgonz671 <jgonz671@ucr.edu>
2024-03-30 01:11:26 -0700

    add hover and icon changes

--- src/data/dynamic/admin/Teams.js
+++ src/data/dynamic/admin/Teams.js
@@ -63,7 +63,11 @@ export const COLUMNS = [
     cell: ({ getValue }) => (
       <div>
         {getValue().map(({ name, link }, index) => (
-          <Link key={index} href={link} className="mx-2 inline-flex">
+          <Link
+            key={index}
+            href={link}
+            className="mx-2 inline-flex hover:opacity-75"
+          >
             {ICONS[name]}
           </Link>
         ))}
