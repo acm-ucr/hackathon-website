83a71cd2602470b5b5ae2fc2a259039504b31e11
Atharva <147217989+atharvanev@users.noreply.github.com>
2024-04-09 00:03:34 -0700

    implemented digit UI with a more efficent implementation of countdown and fixed bg color

--- src/components/user/Countdown.jsx
+++ src/components/user/Countdown.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import data from "/src/data/Config.js";
 
@@ -8,6 +9,27 @@ const year = parseInt(parts[2]);
 const monthIndex = new Date(Date.parse(month + " 1, " + year)).getMonth() + 1;
 const endDate = new Date(year, monthIndex - 1, day, 23, 59, 59);
 
+const Digits = ({ value, unit }) => {
+  return (
+    <div className="flex flex-col items-center gap-4 last:hidden  sm:last:flex">
+      <div className="flex gap-1 m-3 mb-0 lg:!gap-1">
+        {value
+          .toString()
+          .padStart(2, "0")
+          .split("")
+          .map((digit, index) => (
+            <div
+              className="text-lg lg:text-3xl font-bold text-white bg-white bg-opacity-40 p-3 lg:p-3 rounded-lg"
+              key={index}
+            >
+              {digit}
+            </div>
+          ))}
+      </div>
+      <div className="text-white m-2 mt-0 text-xs ">{unit}</div>
+    </div>
+  );
+};
 const Countdown = () => {
   const [countdown, setCountdown] = useState({
     days: 0,
@@ -34,49 +56,12 @@ const Countdown = () => {
   }, []);
 
   return (
-    <div className="inline-block bg-green-400 mr-3 mb-3 rounded-lg font-bold">
-      <div className="m-2 text-white"> HACKING ENDS IN</div>
-
-      <div className="m-3 mt-0 font flex space-x-4 text-white ">
-        <div className="space-x-2">
-          <div className="bg-opacity-40 bg-white p-3 rounded-md text-2xl min-w-10 inline-block">
-            {countdown.days}
-          </div>
-          <div className=" text-center text-xs font-normal">Days</div>
-        </div>
-        <div>
-          <div className="flex space-x-2">
-            <div className="bg-opacity-40 bg-white p-3 rounded-md text-2xl w-10">
-              {Math.floor(countdown.hours / 10)}
-            </div>
-            <div className="bg-opacity-40 bg-white p-3 rounded-md text-2xl w-10">
-              {countdown.hours % 10}
-            </div>
-          </div>
-          <div className=" text-center text-xs font-normal">Hours</div>
-        </div>
-        <div>
-          <div className="flex space-x-2">
-            <div className="bg-opacity-40 bg-white p-3 rounded-md text-2xl w-10">
-              {Math.floor(countdown.minutes / 10)}{" "}
-            </div>
-            <div className="bg-opacity-40 bg-white p-3 rounded-md text-2xl w-10">
-              {countdown.minutes % 10}{" "}
-            </div>
-          </div>
-          <div className=" text-center text-xs font-normal">Minutes</div>
-        </div>
-        <div>
-          <div className="flex space-x-2">
-            <div className="bg-opacity-40 bg-white p-3 rounded-md text-2xl w-10">
-              {Math.floor(countdown.seconds / 10)}{" "}
-            </div>
-            <div className="bg-opacity-40 bg-white p-3 rounded-md text-2xl w-10">
-              {countdown.seconds % 10}{" "}
-            </div>
-          </div>
-          <div className=" text-center text-xs font-normal">Seconds</div>
-        </div>
+    <div className=" rounded-lg inline-flex-col inline-block bg-hackathon-green-300 mb-3 shadow-xl">
+      <div className="font-bold text-white m-2 mb-0">HACKING ENDS IN</div>
+      <div className="inline-flex font-bold">
+        {Object.entries(countdown).map(([unit, value], index) => (
+          <Digits key={index} unit={unit} value={value} />
+        ))}
       </div>
     </div>
   );
