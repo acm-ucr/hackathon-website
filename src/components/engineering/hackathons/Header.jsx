7ebf3f2daa3e2de58523d890f9f110a4994e379b
fardinzam <fardinzaman51@gmail.com>
2024-04-10 08:35:34 -0700

    added rosehack and designverse

--- src/components/engineering/hackathons/Header.jsx
+++ src/components/engineering/hackathons/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex font-bold justify-center text-hackathon-blue-200 text-5xl">
+    <div className="flex font-bold justify-center text-hackathon-blue-200 text-5xl my-10">
       Hackathons at UCR
     </div>
   );
