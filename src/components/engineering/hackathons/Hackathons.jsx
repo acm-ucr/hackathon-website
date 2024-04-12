be120e3208391fdc6774605e80274e36045ddca2
shahdivyank <divyank.shah.2016@gmail.com>
2024-04-12 12:27:15 -0700

    cleanup

--- src/components/engineering/hackathons/Hackathons.jsx
+++ src/components/engineering/hackathons/Hackathons.jsx
@@ -3,47 +3,12 @@ import Link from "next/link";
 import { HackathonsData } from "@/data/engineering/HackathonsData";
 
 export const Hackathons = () => {
-  const sectionText = (data) => {
-    return (
-      <div className="flex flex-col gap-y-8 justify-center px-[5%]">
-        <Image
-          src={data.logo}
-          objectFit="cover"
-          alt={data.alt2}
-          width={200}
-          height={200}
-        />
-        <div className="text-hackathon-blue-100 text-6xl font-bold">
-          {data.title}
-        </div>
-        <div className="text-2xl">{data.text}</div>
-        <Link
-          className="w-fit rounded bg-hackathon-green-400 px-4 py-1 text-lg text-white"
-          href="/"
-        >
-          Details
-        </Link>
-      </div>
-    );
-  };
-  const sectionImg = (data) => {
-    return (
-      <Image
-        src={data.image}
-        objectFit="cover"
-        alt={data.alt1}
-        width={600}
-        height={600}
-      />
-    );
-  };
-
   return (
     <>
       <div className="flex font-bold justify-center text-hackathon-blue-200 text-5xl my-10">
         Hackathons at UCR
       </div>
-      {Object.values(HackathonsData).map((data, index) => (
+      {HackathonsData.map((data, index) => (
         <div
           key={index}
           className={`flex relative ${
@@ -52,8 +17,32 @@ export const Hackathons = () => {
               : "bg-hackathon-gray-100 flex-row-reverse"
           }`}
         >
-          {sectionText(data)}
-          {sectionImg(data)}
+          <div className="flex flex-col gap-y-8 justify-center px-[5%]">
+            <Image
+              src={data.logo}
+              objectFit="cover"
+              alt={data.alt2}
+              width={200}
+              height={200}
+            />
+            <div className="text-hackathon-blue-100 text-6xl font-bold">
+              {data.title}
+            </div>
+            <div className="text-2xl">{data.text}</div>
+            <Link
+              className="w-fit rounded bg-hackathon-green-400 px-4 py-1 text-lg text-white"
+              href="/"
+            >
+              Details
+            </Link>
+          </div>
+          <Image
+            src={data.image}
+            objectFit="cover"
+            alt={data.alt1}
+            width={600}
+            height={600}
+          />
         </div>
       ))}
       <div className="w-full h-52 bg-hackathon-blue-200" />
--- src/data/engineering/HackathonsData.js
+++ src/data/engineering/HackathonsData.js
@@ -1,6 +1,6 @@
-export const HackathonsData = {
-  CutieHack: {
-    id: 1,
+export const HackathonsData = [
+  {
+    name: "CutieHack",
     image: "/engineering/hackathon/cutie.webp",
     alt1: "Photo of hackathon participants",
     logo: "/engineering/hackathon/cutielogo.webp",
@@ -8,8 +8,8 @@ export const HackathonsData = {
     title: "Cutiehack",
     text: "Cutie Hack is a 12-hour, beginner-oriented hackathon hosted by students at University of California, Riverside where hackers are challenged to create a cool project within the time frame to demo in order to win awesome prizes and participate in workshops, fun games, and networking.",
   },
-  RoseHack: {
-    id: 2,
+  {
+    name: "RoseHack",
     image: "/engineering/hackathon/rose.webp",
     alt1: "Photo of hackathon speaker",
     logo: "/engineering/hackathon/roselogo.webp",
@@ -17,8 +17,8 @@ export const HackathonsData = {
     title: "Rosehack",
     text: "Rose Hack is UC Riverside's 24-hour hackathon, one of the few women-centric hackathons in California! Founded by the female leaders of the Association for Computing Machinery (ACM-W) and the Society of Women Engineers (SWE).",
   },
-  DesignVerse: {
-    id: 3,
+  {
+    name: "DesignVerse",
     image: "/engineering/hackathon/design.webp",
     alt1: "Photo of hackathon participants",
     logo: "/engineering/hackathon/designlogo.webp",
@@ -26,8 +26,8 @@ export const HackathonsData = {
     title: "DesignVerse",
     text: "At DesignVerse, our goal is to empower our community of students who are interested in design-like thinking and provide career developmental opportunities through our workshops, mentorship, and networking events.",
   },
-  CitrusHack: {
-    id: 4,
+  {
+    name: "CitrusHack",
     image: "/engineering/hackathon/citrus.webp",
     alt1: "Photo of hackathon participants",
     logo: "/engineering/hackathon/citruslogo.webp",
@@ -35,8 +35,8 @@ export const HackathonsData = {
     title: "CitrusHack",
     text: "Citrus Hack is a 36-hour hackathon hosted by the University of California, Riverside. Hackers are challenged to create a project on the spot and demo it to compete for prizes. Our goal is to provide learning and networking opportunities for our community of hackers.",
   },
-  BearHack: {
-    id: 5,
+  {
+    name: "BearHack",
     image: "/engineering/hackathon/bear.webp",
     alt1: "Photo of hackathon participants",
     logo: "/engineering/hackathon/bearlogo.webp",
@@ -44,4 +44,4 @@ export const HackathonsData = {
     title: "BearHack",
     text: "Founded by the Biomedical Engineering Society (BMES), Bearhack is a medical-centric hackathon event hosted at University of California, Riverside. Our goal is to provide an opprotunity for hackers to build new projects and expand their networks.",
   },
-};
+];
