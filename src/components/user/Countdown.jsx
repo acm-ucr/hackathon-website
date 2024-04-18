0fa117762025d04aae2ea47550065e6940055db2
DannielK <dannykm003@gmail.com>
2024-04-17 18:50:20 -0700

    removed all instances of importing React

--- src/components/user/Countdown.jsx
+++ src/components/user/Countdown.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import data from "/src/data/Config.js";
 
 const parts = data.end.split(" ");
