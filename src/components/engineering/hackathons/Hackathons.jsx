f9ae92cea8ccd9ea9dbc38c9c57b10efde40635c
Stanley Lew <stanley43595@gmail.com>
2024-04-24 09:40:02 -0700

    responsiveness for hackathons page

--- src/components/engineering/hackathons/Hackathons.jsx
+++ src/components/engineering/hackathons/Hackathons.jsx
@@ -4,8 +4,8 @@ import { HackathonsData } from "@/data/engineering/HackathonsData";
 
 export const Hackathons = () => {
   return (
-    <>
-      <div className="flex font-bold justify-center text-hackathon-blue-200 text-5xl my-10">
+    <div className="text-center md:text-left">
+      <div className="flex font-bold justify-center text-hackathon-blue-200 text-4xl md:text-5xl md:my-10 pt-6 pb-4">
         Hackathons at UCR
       </div>
       {HackathonsData.map((data, index) => (
@@ -13,24 +13,25 @@ export const Hackathons = () => {
           key={index}
           className={`flex relative ${
             data.id % 2 === 0
-              ? "bg-white flex-row "
-              : "bg-hackathon-gray-100 flex-row-reverse"
+              ? "bg-white flex-col md:flex-row "
+              : "bg-hackathon-gray-100 flex-col md:flex-row-reverse"
           }`}
         >
-          <div className="flex flex-col gap-y-8 justify-center px-[5%]">
+          <div className="flex flex-col items-center gap-y-4 md:gap-y-8 justify-center px-[5%]">
             <Image
               src={data.logo}
               objectFit="cover"
               alt={data.alt2}
               width={200}
               height={200}
+              className="mt-4"
             />
-            <div className="text-hackathon-blue-100 text-6xl font-bold">
+            <div className="text-hackathon-blue-100 text-5xl md:text-6xl font-bold">
               {data.title}
             </div>
-            <div className="text-2xl">{data.text}</div>
+            <div className="text-xl md:text-2xl">{data.text}</div>
             <Link
-              className="w-fit rounded bg-hackathon-green-400 px-4 py-1 text-lg text-white"
+              className="w-fit rounded bg-hackathon-green-400 px-4 py-1 text-lg text-white mb-4"
               href="/"
             >
               Details
@@ -46,7 +47,7 @@ export const Hackathons = () => {
         </div>
       ))}
       <div className="w-full h-52 bg-hackathon-blue-200" />
-    </>
+    </div>
   );
 };
 
