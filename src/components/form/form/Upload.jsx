b6e14c7a24d502db92e49ea3e170ea665813a3e4
Arslan Asif <malikarslanasif131@gmail.com>
2024-05-28 13:06:51 +0500

    Add padding below 'Upload From My Computer' button (fixes #1329)

--- src/components/form/form/Upload.jsx
+++ src/components/form/form/Upload.jsx
@@ -46,7 +46,7 @@ const Upload = ({ field, user, setUser, text, maxSize, types, required }) => {
             htmlFor="dropzone-file"
             className="flex flex-col items-center justify-center w-full h-fit border-2 border-gray-300 rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
           >
-            <div className="flex flex-col items-center justify-center pt-4">
+            <div className="flex flex-col items-center justify-center pt-4 pb-4">
               <BsUpload className=" text-3xl mb-2 text-hackathon-green-300" />
               <p className="text-sm text-gray-500 font-semibold">
                 Upload from my computer
