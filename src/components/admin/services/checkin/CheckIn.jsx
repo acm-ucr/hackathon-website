ade682136cc3d44915d83fc6c41054761166cf4b
jmacl001 <jmacl001@ucr.edu>
2024-06-20 19:08:39 -0700

    Modify component div class from p-3 to md:p-3 for Checkin

--- src/components/admin/services/checkin/CheckIn.jsx
+++ src/components/admin/services/checkin/CheckIn.jsx
@@ -72,7 +72,7 @@ const CheckIn = () => {
     <div className="h-full font-poppins flex flex-col py-4 gap-3">
       <Title title="Check In" />
       <div className="grid grid-cols-1">
-        <div className="p-3 flex flex-col items-center">
+        <div className="md:p-3 flex flex-col items-center">
           {events && (
             <Dropdown
               option={event}
