9b5c44b28339d898bed7218e70a464b129a3f919
kellyma073 <kma073@ucr.edu>
2024-06-21 02:15:17 -0700

    added page titles

--- src/data/engineering/Navigation.js
+++ src/data/engineering/Navigation.js
@@ -2,9 +2,11 @@ export const TABS = [
   {
     label: "hackathons",
     link: "/engineering/hackathons",
+    title: "Bearhack Engineering | Hackathons",
   },
   {
     label: "blogs",
     link: "/engineering/blog",
+    title: "Bearhack Engineering | Blogs",
   },
 ];
