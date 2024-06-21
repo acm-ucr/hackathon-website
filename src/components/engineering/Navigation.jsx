cf95af3629fc8979c4c4f5d53e7ed5337d5a4fb8
kellyma073 <kma073@ucr.edu>
2024-06-21 02:57:08 -0700

    pg titles

--- src/components/engineering/Navigation.jsx
+++ src/components/engineering/Navigation.jsx
@@ -12,6 +12,7 @@ const Navigation = () => {
             key={index}
             href={tab.link}
             className="mx-6 hover:text-white/50 duration-300"
+            title={tab.title}
           >
             {tab.label}
           </Link>
