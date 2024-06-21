925c2831ca57dc391818aeb8f5c0336b4a1328cc
Jonathan Le <jonathanle062303@gmail.com>
2024-06-20 19:28:53 -0700

    Update date in form descriptions

--- src/data/form/Panelists.js
+++ src/data/form/Panelists.js
@@ -14,7 +14,17 @@ export const FIELDS = {
     input: "description",
     width: 12,
     texts: [
-      `Welcome to ${data.name}. Thank you for considering to become a panelist. We appreciate your efforts to help support ${data.name}. ${data.name} is a ${data.description} hackathon spanning ${data.length} on ${data.date}.`,
+      `Welcome to ${
+        data.name
+      }. Thank you for considering to become a panelist. We appreciate your efforts to help support ${
+        data.name
+      }. ${data.name} is a ${data.description} hackathon spanning ${
+        data.length
+      } on ${data.date.toLocaleString("default", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      })}.`,
       "Panelists are not required to stay the full duration of the event, but are encouraged to checkout the various events, workshops, and opportunities that are available.",
     ],
   },
--- src/data/form/Volunteers.js
+++ src/data/form/Volunteers.js
@@ -7,7 +7,17 @@ export const FIELDS = {
     input: "description",
     width: 12,
     texts: [
-      `Welcome to ${data.name}. Thank you for considering to become a volunteer, we appreciate your efforts to help support ${data.name}. ${data.name} is a ${data.description} hackathon spanning ${data.length} on ${data.date}.`,
+      `Welcome to ${
+        data.name
+      }. Thank you for considering to become a volunteer, we appreciate your efforts to help support ${
+        data.name
+      }. ${data.name} is a ${data.description} hackathon spanning ${
+        data.length
+      } on ${data.date.toLocaleString("default", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      })}.`,
       "Volunteers are not required to stay the full duration of the event, but are encouraged to checkout the various events, workshops, and opportunities that are available.",
       "Volunteer duties include but are not limited to setup, workshop organization, food handling, cleanup and more.",
       "Note: Volunteers are not permitted to become participants for the hackathon.",
