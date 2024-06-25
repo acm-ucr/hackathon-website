29bf4f3bef61222ec4bfa5591dd0511711141be4
Jasmine Trình <168297703+minnieland@users.noreply.github.com>
2024-06-25 15:23:12 -0700

    checked for proper text

--- cypress/component/form/Confirmation.jsx
+++ cypress/component/form/Confirmation.jsx
@@ -3,9 +3,10 @@ import Confirmation from "@/components/form/form/Confirmation.jsx";
 
 describe("Confirmation", () => {
   it("should render and display correct message", () => {
-    const expectedText = "Thank you for applying!";
+    const expectedText =
+      "Thank you for filling out the application form. Please watch out for an email with an application status update.";
 
-    cy.mount(<Confirmation message={expectedText} />);
+    cy.mount(<Confirmation />);
     cy.get('[data-cy="confirmation-message"]').should("exist");
     cy.get('[data-cy="confirmation-message"]').should(
       "have.text",
