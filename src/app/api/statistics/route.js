c464109c5fbc48a5f32a380e8921e749a110fe33
nidheesh-m-vakharia <nvakh003@ucr.edu>
2024-06-25 04:29:48 -0700

    Removed catch and then

--- src/app/api/statistics/route.js
+++ src/app/api/statistics/route.js
@@ -6,63 +6,64 @@ import { authenticate } from "@/utils/auth";
 export const GET = async () => {
   const res = NextResponse;
 
-  return authenticate({ admins: [1] })
-    .then(({ auth }) => {
-      if (auth !== 200) {
-        return res.json(
-          {
-            message: `Authentication Error: ${"MESSAGE VARIABLE SHOULD BE HERE"}`,
-          },
-          { status: auth }
-        );
-      }
+  const { auth } = await authenticate({
+    admins: [1],
+  });
 
-      const statisticsPromise = getDoc(doc(db, "statistics", "statistics"));
-      const eventsPromise = getDocs(collection(db, "events"));
+  if (auth !== 200) {
+    return res.json(
+      { message: `Authentication Error: ${"MESSAGE VARIABLE SHOULD BE HERE"}` },
+      { status: auth }
+    );
+  }
 
-      return Promise.all([statisticsPromise, eventsPromise]);
-    })
-    .then(([statisticsSnapshot, eventsSnapshot]) => {
-      const statisticsData = statisticsSnapshot.data();
-      const {
-        teams: { 1: teams },
-        participants: { 1: participants },
-        volunteers: { 1: volunteers },
-        judges: { 1: judges },
-        mentors: { 1: mentors },
-        committees: { 1: committees },
-        sponsors: { 1: sponsors },
-        panels: { 1: panels },
-        admins: { 1: admins },
-      } = statisticsData;
+  try {
+    const statisticsDocRef = doc(db, "statistics", "statistics");
+    const eventsCollectionRef = collection(db, "events");
 
-      const eventAttendees = {};
-      eventsSnapshot.forEach((doc) => {
-        const { name, attendance } = doc.data();
-        eventAttendees[name] = attendance;
-      });
+    const [statisticsDoc, eventsSnapshot] = await Promise.all([
+      getDoc(statisticsDocRef),
+      getDocs(eventsCollectionRef),
+    ]);
 
-      const users = {
-        participants,
-        teams,
-        judges,
-        volunteers,
-        mentors,
-        committees,
-        sponsors,
-        panels,
-        admins,
-      };
+    const {
+      teams: { 1: teams },
+      participants: { 1: participants },
+      volunteers: { 1: volunteers },
+      judges: { 1: judges },
+      mentors: { 1: mentors },
+      committees: { 1: committees },
+      sponsors: { 1: sponsors },
+      panels: { 1: panels },
+      admins: { 1: admins },
+    } = statisticsDoc.data();
 
-      return res.json(
-        { items: { users, events: eventAttendees } },
-        { status: 200 }
-      );
-    })
-    .catch((err) => {
-      return res.json(
-        { message: `Internal Server Error: ${err}` },
-        { status: 500 }
-      );
+    const eventAttendees = {};
+    eventsSnapshot.forEach((doc) => {
+      const { name, attendance } = doc.data();
+      eventAttendees[name] = attendance;
     });
+
+    const users = {
+      participants,
+      teams,
+      judges,
+      volunteers,
+      mentors,
+      committees,
+      sponsors,
+      panels,
+      admins,
+    };
+
+    return res.json(
+      { items: { users, events: eventAttendees } },
+      { status: 200 }
+    );
+  } catch (err) {
+    return res.json(
+      { message: `Internal Server Error: ${err}` },
+      { status: 500 }
+    );
+  }
 };
