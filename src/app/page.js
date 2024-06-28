29493824363f011ddb8dbc878665394a72903265
shahdivyank <divyank.shah.2016@gmail.com>
2024-06-27 23:39:29 -0700

    remove postlive

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,12 @@
 import Release from "@/components/Release";
 import RELEASES from "@/data/Releases";
 import Live from "@/components/live/Live";
-import Postlive from "@/components/postlive/Postlive";
 
 const Home = () => {
   return (
     <div className="w-full">
       <Release release={RELEASES.LIVE}>
         <Live />
-      </Release>
-      <Release release={RELEASES.POSTLIVE}>
-        <Postlive />
       </Release>
     </div>
   );
