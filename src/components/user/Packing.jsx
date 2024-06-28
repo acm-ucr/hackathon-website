00ece0b9a58c19d68e2aafac5a527e3b6aed3249
Nolan Kosmal <Nolan.kosmal@gmail.com>
2024-06-27 21:06:01 -0700

    Added to packing List

--- src/components/user/Packing.jsx
+++ src/components/user/Packing.jsx
@@ -2,9 +2,13 @@ const Packing = () => {
   const list = [
     "Computer and accessories (mouse, keyboard, etc.)",
     "Chargers (phone, laptop, etc.)",
-    "Hardware that you might want to hack on.",
+    "Hardware that you might want to hack on or incorporate into your build.",
     "Sleep-related things (sleeping bags, pillow, blankets, etc.)",
     "There will be a resting area within the venue itself, but we can't provide sleeping materials to hackers.",
+    "A change of clothes (including something warm such as a jacket or sweatshirt)",
+    "Toiletries:",
+    "Medication (if needed)",
+    "Refillable water bottle",
   ];
   return (
     <div className="bg-white rounded-lg shadow-xl p-3 my-3">
@@ -14,7 +18,15 @@ const Packing = () => {
 
       <ul className="font-light list-disc pl-9 mb-2" data-cy="packing-items">
         {list.map((list, index) => (
-          <li key={index}>{list}</li>
+          <li key={index}>
+            {list}
+            {list == "Toiletries:" && (
+              <ul className="list-disc pl-9">
+                <li>ToothBrush / toothpaste</li>
+                <li>Shower Supplies (soap, towel, shower shoes)</li>
+              </ul>
+            )}
+          </li>
         ))}
       </ul>
     </div>
