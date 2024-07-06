2a3cc1763c1bc6897468ecdfe79cc0df86f0c23a
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-07-06 01:10:45 -0700

    current state of user schedule

--- src/components/user/schedule/Events.jsx
+++ src/components/user/schedule/Events.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 
-const Events = () => {
-  return <div>Events</div>;
+// const colors = {
+//   general: {
+//     bg: "bg-hackathon-blue-100",
+//     text: "text-hackathon-blue-100",
+//     border: "border-hackathon-blue-100",
+//   },
+//   food: {
+//     bg: "bg-hackathon-blue-100",
+//     text: "text-hackathon-blue-100",
+//     border: "border-hackathon-blue-100",
+//   },
+//   hackathon: { bg: "bg-white", text: "text-white", border: "border-white" },
+//   workshop: {
+//     bg: "bg-purple-300",
+//     text: "text-purple-300",
+//     border: "border-purple-300",
+//   },
+// };
+
+const Events = ({ event }) => {
+  return (
+    <div className="bg-white flex flex-col p-3 rounded-lg w-full shadow-sm">
+      <div className="font-bold text-sm">{event.summary}</div>
+      <div className="flex gap-0 items-center justify-center w-full py-1">
+        <div
+          className={`${
+            event.start < new Date()
+              ? event.end > new Date()
+                ? "bg-white/30 animate-bounce"
+                : "opacity-70"
+              : "bg-white/20"
+          } py-2 flex w-full`}
+        >
+          <div className={`text-center text-xs md:text-sm`}>
+            {event.start.getHours() === 12 ? 12 : event.start.getHours() % 12}:
+            {event.start.getMinutes() < 10 && "0"}
+            {event.start.getMinutes()}{" "}
+            {event.start.getHours() >= 12 ? "PM " : "AM "}
+          </div>
+        </div>
+
+        <div className="w-full text-center text-xs md:text-sm">
+          {event.location}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Events;
--- src/components/user/schedule/Schedule.jsx
+++ src/components/user/schedule/Schedule.jsx
@@ -1,17 +1,54 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Toolbar from "./Toolbar";
 import Events from "./Events";
+import { api } from "@/utils/api";
+
+const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
 
 const Schedule = () => {
-  const [filteredEvents, setFilteredEvents] = useState(mockEvents);
-
+  const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  useEffect(() => {
+    api({
+      method: "GET",
+      url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`,
+    })
+      .then((response) => {
+        // let selected = null;
+        const items = response.items.map((item) => {
+          item.start = new Date(item.start.dateTime);
+          item.end = new Date(item.end.dateTime);
+          item.day = item.start.getDay();
+          // const [category] = item.description
+          //   .split("\n")[0]
+          //   .split("#")
+          //   .map((item) => item.trim())
+          //   .filter((item) => item !== "");
+          // item.category = category;
+          // if (item.description.includes("http")) {
+          //   const startIndex = item.description.indexOf("http");
+          //   item.link = item.description.substring(startIndex);
+          // }
+          // item.description = item.description?.split("\n")[1];
+          // if (!selected && item.start >= new Date())
+          //   selected = item.start.getDay();
+          return item;
+        });
+        setEvents(items);
+        setFilteredEvents(items);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch events", error);
+      });
+  }, []);
+  console.log(events);
   const filterChange = (filterType) => {
-    if (filterType === "all") {
-      setFilteredEvents(mockEvents);
+    if (filterType === "general") {
+      setFilteredEvents(events);
     } else {
       setFilteredEvents(
-        mockEvents.filter((event) => event.type === filterType)
+        events.filter((event) => event.category === filterType)
       );
     }
   };
@@ -19,24 +56,91 @@ const Schedule = () => {
   return (
     <>
       <Toolbar onFilterChange={filterChange} />
-      <div className="mt-4">
-        {filteredEvents.map((event) => (
-          <div key={event.id} className="border p-2 mb-2">
-            {event.title}
+      <div className="flex text-white bg-white/10 w-full bg-red-200">
+        {days.map((day, index) => (
+          <div
+            className="text-black font-montserrat font-light text-sm md:text-lg items-center justify-start flex flex-grow border-b-[1px] border-black m-5 ml-0"
+            key={index}
+          >
+            {day}
           </div>
         ))}
       </div>
-      <Events />
+      <div className="flex mr-4">
+        {days.map((day, dayIndex) => (
+          <div
+            key={dayIndex}
+            className=" flex flex-col items-start w-full space-y-3 m-2 "
+          >
+            {filteredEvents
+              .filter((event) => {
+                if (day === "SUN" && event.day === 0) return true;
+                if (day === "SAT" && event.day === 6) return true;
+                return event.day === dayIndex + 1;
+              })
+              .map((events, eventIndex) => (
+                <Events event={events} key={eventIndex} />
+              ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 };
 
 export default Schedule;
 
-const mockEvents = [
-  { id: 1, title: "Event 1", type: "all" },
-  { id: 2, title: "HackWeek Event", type: "hackweek" },
-  { id: 3, title: "Hackathon Event", type: "hackathon" },
-  { id: 4, title: "Another Event", type: "all" },
-  { id: 5, title: "More HackWeek", type: "hackweek" },
-];
+// const mockEvents = [
+//   {
+//     day: 1,
+//     category: "general",
+//     summary: "mario kart",
+//     location: "my house",
+//     type: "all",
+//     filter: "food",
+//   },
+//   {
+//     day: 2,
+//     category: "general",
+//     summary: "mario kart",
+//     location: "my house",
+//     type: "all",
+//     filter: "social",
+//   },
+//   {
+//     day: 4,
+//     category: "general",
+//     summary: "mario krt",
+//     location: "my house",
+//     type: "all",
+//     filter: "food",
+//   },
+//   {
+//     day: 5,
+//     category: "general",
+//     summary: "mario kart",
+//     location: "my house",
+//     type: "hackathon",
+//     filter: "food",
+//   },
+//   {
+//     day: 6,
+//     category: "food",
+//     summary: "mario kart",
+//     location: "my house",
+//     type: "hackweek",
+//     filter: "food",
+//   },
+//   {
+//     day: 3,
+//     category: "workshop",
+//     summary: "mario kart",
+//     location: "my house",
+//     type: "hackweek",
+//     filter: "meeting",
+//   },
+//   // { id: 2, title: "HackWeek Event", type: "hackweek" },
+//   // { id: 3, title: "Hackathon Event", type: "hackathon" },
+//   // { id: 4, title: "Another Event", type: "all" },
+//   // { id: 5, title: "More HackWeek", type: "hackweek" },
+// ];
