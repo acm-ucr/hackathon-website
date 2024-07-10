cdb2ad2bec665ecc3960b7f69bca5783f1d7596a
mhhg1030 <huonglm3010@gmail.com>
2024-07-10 14:13:55 -0700

    create loading component test

---
+++ src/components/Loading.cy.jsx
@@ -0,0 +1,20 @@
+import Loading from "../../src/components/Loading";
+
+describe("Loading Component", () => {
+  it("renders Loading component correctly", () => {
+    // Mount the Loading component
+    mount(<Loading />);
+
+    // test "loading..." text and style
+    cy.contains("Loading...")
+      .should("exist")
+      .and("have.class", "text-3xl")
+      .and("have.class", "font-bold")
+      .and("have.class", "text-hackathon-blue-100");
+
+    // test spinning thing
+    cy.get(".animate-spin")
+      .should("exist")
+      .and("have.class", "text-hackathon-blue-100");
+  });
+});
