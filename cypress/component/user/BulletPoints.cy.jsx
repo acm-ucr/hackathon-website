9d158faa082bc2e5b191697715ff1c14123ddb75
lowbabun <chsu115@ucr.edu>
2024-07-11 16:52:18 -0700

    better variable names

--- cypress/component/user/BulletPoints.cy.jsx
+++ cypress/component/user/BulletPoints.cy.jsx
@@ -1,17 +1,17 @@
 import BulletPoints from "@/components/user/BulletPoints";
 
-describe("BulletPoints Component", () => {
-  it("renders BulletPoints component correctly", () => {
+describe("BulletPoints", () => {
+  it("BulletPoints", () => {
     const classes = "flex flex-col items-center justify-center mt-2";
     const listClass = "list-disc w-10/12";
     const list = ["first item", "second item"];
 
     cy.mount(<BulletPoints list={list} />);
 
-    cy.get("[data-cy=bulletpoint-component]").should("have.class", classes);
+    cy.get("[data-cy=bulletpoint-list-items]").should("have.class", classes);
 
     list.forEach((item, index) => {
-      cy.get(`[data-cy=bulletpoint-component] [data-cy=list-component]`)
+      cy.get(`[data-cy=bulletpoint-list-items] [data-cy=bulletpoint-list-item]`)
         .eq(index)
         .should("contain.text", item)
         .and("have.class", listClass);
--- src/components/user/BulletPoints.jsx
+++ src/components/user/BulletPoints.jsx
@@ -1,11 +1,15 @@
 const BulletPoints = ({ list }) => {
   return (
     <ul
-      data-cy="bulletpoint-component"
+      data-cy="bulletpoint-list-items"
       className="flex flex-col items-center justify-center mt-2"
     >
       {list.map((item, index) => (
-        <li data-cy="list-component" key={index} className="list-disc w-10/12">
+        <li
+          data-cy="bulletpoint-list-item"
+          key={index}
+          className="list-disc w-10/12"
+        >
           {item}
         </li>
       ))}
