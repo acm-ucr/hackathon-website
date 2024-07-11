1ca990c17904f5a0d032989d4adccf301a36cd9b
mhhg1030 <huonglm3010@gmail.com>
2024-07-10 19:45:23 -0700

    fixed code for loading component test

--- src/components/Loading.cy.jsx
+++ src/components/Loading.cy.jsx
@@ -2,17 +2,12 @@ import Loading from "../../src/components/Loading";
 
 describe("Loading Component", () => {
   it("renders Loading component correctly", () => {
-    // Mount the Loading component
     cy.mount(<Loading />);
 
-    // test "loading..." text and style
     cy.contains("Loading...")
       .should("exist")
-      .and("have.class", "text-3xl")
-      .and("have.class", "font-bold")
-      .and("have.class", "text-hackathon-blue-100");
+      .and("have.class", "text-3xl", "font-bold", "text-hackathon-blue-100")
 
-    // test spinning thing
     cy.get(".animate-spin")
       .should("exist")
       .and("have.class", "text-hackathon-blue-100");
