e6ee18a6e1046922d8359182e98f01ff8b51b095
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-07-12 22:37:41 -0700

    implemented shadcn checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,4 +1,4 @@
-import { BsCheckLg } from "react-icons/bs";
+import { Checkbox as Check } from "./ui/checkbox";
 
 const Checkbox = ({ toggle, onClick = () => {}, text = "", color }) => {
   return (
@@ -7,21 +7,15 @@ const Checkbox = ({ toggle, onClick = () => {}, text = "", color }) => {
       onClick={onClick}
       data-cy="checkbox"
     >
-      <div
-        data-cy="checkbox-bg"
+      <Check
+        checked={toggle}
+        onChange={onClick}
         className={`w-4 h-4 rounded-sm mr-2 ${
           toggle
             ? `${color ? color : "bg-hackathon-blue-100"}`
             : "bg-hackathon-gray-100"
-        } flex items-center justify-center`}
-      >
-        <BsCheckLg
-          data-cy="checkmark"
-          className={`${
-            toggle ? "text-white" : "text-hackathon-gray-100"
-          } text-lg`}
-        />
-      </div>
+        }`}
+      />
       {text && <p className="my-0 pt-0">{text}</p>}
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/data/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         poppins: ["var(--font-poppins)"],
@@ -47,7 +56,21 @@ module.exports = {
           },
         },
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("tailwindcss-animate")],
 };
