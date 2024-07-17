c66d3b7e9b170f72c7305b52137bb0c0977ad2b1
Jonathan Le <jonathanle062303@gmail.com>
2024-07-17 00:30:03 -0700

    Add responsiveness to scanner

--- src/components/admin/services/checkin/Scanner.jsx
+++ src/components/admin/services/checkin/Scanner.jsx
@@ -7,7 +7,12 @@ const ScanQRCode = ({ setResult }) => {
     },
   });
 
-  return <video ref={ref} className="md:w-1/2 aspect-square w-full" />;
+  return (
+    <video
+      ref={ref}
+      className="2xl:w-1/2 xl:w-7/12 lg:w-3/4 aspect-square w-full"
+    />
+  );
 };
 
 export default ScanQRCode;
