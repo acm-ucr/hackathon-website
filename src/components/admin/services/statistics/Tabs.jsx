729a4964645d15a03e3d6a4a627ce1a05cb51315
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-07-16 23:25:51 -0700

    removed counts

--- src/components/admin/services/statistics/Tabs.jsx
+++ src/components/admin/services/statistics/Tabs.jsx
@@ -3,11 +3,6 @@ import Tab from "./Tab";
 const Tabs = ({ counts, events }) => {
   return (
     <>
-      <div className="w-full mt-3 grid md:grid-cols-7 grid-cols-2 p-4 gap-4">
-        {Object.entries(counts).map(([title, count], index) => (
-          <Tab key={index} title={title} value={count["1"]} />
-        ))}
-      </div>
       <div className="w-full mt-3 grid md:grid-cols-4 grid-cols-1 p-4 gap-4">
         {Object.entries(events).map(([title, count], index) => (
           <Tab key={index} title={title} value={count} />
