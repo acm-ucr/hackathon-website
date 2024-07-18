ebd11b1f4e7599d04f3f2f8980b96b8fe18cc377
DannielK <dannykm003@gmail.com>
2024-07-18 13:19:33 -0700

    Updated Rules

--- src/data/user/Rules.js
+++ src/data/user/Rules.js
@@ -1,8 +1,11 @@
 export const RULES = [
   "You may not submit a project that has already been submitted or started before our hackathon period.",
-  "Your project must be an original and not copied from someone else's work.",
+  "Your project must be an original and not copied from someone else's work. We check for any large copy/paste contributions.",
   "Teams of 1-4 are required.",
   "A Github is required for submission.",
-  "Github commits may not be dated before the hackathon period has started.",
+  "We check all Github repos to ensure no Github commits are dated before or after the hackathon period has started.",
   "We will not judge any late projects.",
+  "All members of your team must have checked-in to the event.",
+  "At least one member of your team must be physically present to demo your project to the judges.",
+  "Ideation and planning are allowed (and encouraged) before the event, but all coding/building must take place during hacking hours.",
 ];
