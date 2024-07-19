9d4031d6c77b489c70ab610b5ce1b7888b85aa7b
Rama Chunduri <chunduri156@gmail.com>
2024-07-19 10:57:47 +0530

    spacing toolbar

--- src/components/admin/services/judging/Toolbar.jsx
+++ src/components/admin/services/judging/Toolbar.jsx
@@ -198,9 +198,9 @@ const Toolbar = ({
           text="add judges"
         />
       )}
-      <div className="flex items-center justify-between w-full">
-        <div className="flex items-center ">
-          <form className="flex items-center pr-2" onSubmit={generate}>
+      <form className="flex items-center pr-2" onSubmit={generate}>
+        <div className="flex flex-col sm:flex-row sm:items-center justify-between w-full">
+          <div className="flex flex-row pl-2 pb-3">
             <Input
               setObject={setInput}
               object={input}
@@ -209,18 +209,26 @@ const Toolbar = ({
               maxLength={2}
               placeholder="ie. 5"
               clear={true}
-              classes="w-20"
             />
             <p className="mb-0 font-semibold mx-2"># of rotations</p>
-            <Button color="green" text="generate" onClick={generate} />
-          </form>
-          <Button
-            color="red"
-            text="reset"
-            onClick={handleReset}
-            disabled={!data || data.some(({ rounds }) => rounds.length === 0)}
-          />
-          <div className="pl-2">
+          </div>
+
+          <div className="flex flex-row pb-3 pl-2">
+            <div>
+              <Button color="green" text="generate" onClick={generate} />
+            </div>
+
+            <div>
+              <Button
+                color="red"
+                text="reset"
+                onClick={handleReset}
+                disabled={
+                  !data || data.some(({ rounds }) => rounds.length === 0)
+                }
+              />
+            </div>
+
             <Button
               color="green"
               text="change view"
@@ -228,7 +236,7 @@ const Toolbar = ({
               disabled={!data || data.some(({ rounds }) => rounds.length === 0)}
             />
           </div>
-          <div className="pl-2">
+          <div className="pl-2 pb-3">
             <Input
               value={search}
               label="search"
@@ -240,13 +248,14 @@ const Toolbar = ({
               onChangeFn={handleInput}
             />
           </div>
+
+          <div className="flex flex-row">
+            {tags.map((tag, index) => (
+              <Tag key={index} color={COLORS[tag]} text={tag} classes="mx-2" />
+            ))}
+          </div>
         </div>
-        <div className="flex">
-          {tags.map((tag, index) => (
-            <Tag key={index} color={COLORS[tag]} text={tag} classes="mx-2" />
-          ))}
-        </div>
-      </div>
+      </form>
     </>
   );
 };
