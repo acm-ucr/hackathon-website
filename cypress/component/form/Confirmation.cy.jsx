9cf5e69fdf930997093d409825eaa2d94ec1435b
shahdivyank <divyank.shah.2016@gmail.com>
2024-07-21 03:05:23 -0700

    Revert "remove jsx ending"
    This reverts commit 7513d7dcc8389c09e042867cbd7522664d5d0c69.

--- cypress/component/form/Confirmation.cy.jsx
+++ cypress/component/form/Confirmation.cy.jsx
@@ -1,4 +1,4 @@
-import Confirmation from "@/components/form/form/Confirmation";
+import Confirmation from "@/components/form/form/Confirmation.jsx";
 
 describe("Confirmation", () => {
   it("should render and display correct message", () => {
