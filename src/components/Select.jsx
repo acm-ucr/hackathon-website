fd4510a45b283ccba546663a101cb07930bc282f
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-07-23 23:16:10 -0700

    WHY DOES IT NOT SHOW THE ITEMS AAAA

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-// import { useVirtualizer } from "@tanstack/react-virtual";
+import { useVirtualizer } from "@tanstack/react-virtual";
 
 import {
   Command,
@@ -25,19 +25,25 @@ const Select = ({
   title,
   required,
 }) => {
-  const [options] = useState(items);
+  const [filtered, setFiltered] = useState(items);
   const [open, setOpen] = useState(false);
   const [current, setCurrent] = useState(null);
   const parent = useRef(null);
-  // const { getTotalSize, getVirtualItems, measureElement } = useVirtualizer({
-  //   count: options.length,
-  //   getScrollElement: () => ref.current,
-  //   estimateSize: () => 50,
-  //   measureElement: (el) => {
-  //     if (el.innerText.length > 45) return 50;
-  //     else return 35;
-  //   },
-  // });
+  const { getTotalSize, getVirtualItems, measureElement } = useVirtualizer({
+    count: filtered.length,
+    getScrollElement: () => parent.current,
+    estimateSize: () => 50,
+  });
+
+  const handleSearch = (search) => {
+    setFiltered(
+      items.filter((item) =>
+        item.toLowerCase().includes(search.toLowerCase() ?? [])
+      )
+    );
+  };
+
+  console.log(getVirtualItems());
 
   return (
     <div>
@@ -57,25 +63,51 @@ const Select = ({
             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-96 p-0 text-lg ">
-          <Command>
-            <CommandInput placeholder="search" className="text-lg " />
+        <PopoverContent className="w-[750px] p-0 text-lg ">
+          <Command shouldFilter={false}>
+            <CommandInput
+              onValueChange={handleSearch}
+              placeholder="search"
+              className="text-lg "
+            />
             <CommandEmpty>No Options Available.</CommandEmpty>
             <CommandList ref={parent}>
-              {options.map((option, index) => (
-                <CommandItem
-                  key={index}
-                  value={option}
-                  className="text-lg"
-                  onSelect={(value) => {
-                    setCurrent(current === value ? null : option);
-                    setUser({ ...user, [field]: option });
-                    setOpen(false);
-                  }}
-                >
-                  {option}
-                </CommandItem>
-              ))}
+              <div
+                style={{
+                  height: `${getTotalSize()}px`,
+                  width: "100%",
+                  position: "relative",
+                }}
+              >
+                {getVirtualItems().map((virtualItem) => {
+                  const name = filtered[virtualItem.index];
+
+                  return (
+                    <CommandItem
+                      key={name}
+                      data-index={virtualItem.index}
+                      value={name}
+                      className="text-lg"
+                      onSelect={(value) => {
+                        setCurrent(current === value ? null : name);
+                        setUser({ ...user, [field]: name });
+                        setOpen(false);
+                      }}
+                      ref={measureElement}
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: `${virtualItem.size}px`,
+                        transform: `translateY(${virtualItem.start}px)`,
+                      }}
+                    >
+                      {name}
+                    </CommandItem>
+                  );
+                })}
+              </div>
             </CommandList>
           </Command>
         </PopoverContent>
