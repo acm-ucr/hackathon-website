bc9c75e2f41b3fbd6fdc270e2ca79498461a1de3
fardinzam <fardinzaman51@gmail.com>
2024-07-25 17:53:18 -0700

    removed cursor animation on hover

--- src/components/user/BulletList.jsx
+++ src/components/user/BulletList.jsx
@@ -1,7 +1,7 @@
 const BulletList = ({ text, children }) => {
   return (
     <div className="w-full bg-white rounded-xl p-4 drop-shadow-md">
-      <div className="flex items-center justify-between cursor-pointer text-xl font-bold">
+      <div className="flex items-center justify-between text-xl font-bold">
         {text}
       </div>
       <div className=" max-h-[1000px] overflow-hidden">{children}</div>
