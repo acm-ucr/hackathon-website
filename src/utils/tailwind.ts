07a08908fdeab88a1ee8fc8d4dc2a01f9679ef0e
Jonathan Le <jonathanle062303@gmail.com>
2024-07-26 05:42:42 -0700

    Convert Tailwind util to Typescript

--- src/utils/tailwind.ts
+++ src/utils/tailwind.ts
@@ -1,4 +1,4 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const cn = (...inputs) => twMerge(clsx(inputs));
+export const cn = (...inputs: string[]) => twMerge(clsx(inputs));
