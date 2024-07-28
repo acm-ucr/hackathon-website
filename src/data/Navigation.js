179c844034e29c168b4a58af8e0a13a1d97019c6
sunbrandon <sunabrandon@gmail.com>
2024-07-28 14:39:37 -0700

    Testing revert to react-icons

--- src/data/Navigation.js
+++ src/data/Navigation.js
@@ -1,24 +1,25 @@
 import {
-  UserFill,
-  UsersFill,
-  BarChart2,
-  HandRaised,
-  HandHeart,
-  MessageSquare,
-  HelpCircle,
-  Badge,
-  Gavel,
-  Mail,
-  Lock,
-  Calendar,
-  AlertTriangle,
-  Search,
-  Settings,
-  QrCode,
-  Handshake,
-  Users,
-  People,
-} from "lucide-react";
+  BsFillPersonFill,
+  BsPeopleFill,
+  BsBarChartFill,
+  BsPersonRaisedHand,
+} from "react-icons/bs";
+import { MdVolunteerActivism, MdFeedback } from "react-icons/md";
+import { BiSolidHelpCircle } from "react-icons/bi";
+import { GoSponsorTiers } from "react-icons/go";
+import {
+  FaGavel,
+  FaEnvelope,
+  FaLock,
+  FaCalendarDay,
+  FaExclamation,
+  FaSearch,
+} from "react-icons/fa";
+import { FaGear } from "react-icons/fa6";
+import { AiOutlineQrcode } from "react-icons/ai";
+import { SiHandshake } from "react-icons/si";
+import { RiTeamFill } from "react-icons/ri";
+import { IoIosPeople } from "react-icons/io";
 
 const iconStyle = "mr-2";
 
@@ -32,62 +33,62 @@ export const TABS = {
         {
           name: "participants",
           link: "/admin/participants",
-          icon: <UserFill className={iconStyle} />,
+          icon: <BsFillPersonFill className={iconStyle} />,
         },
         {
           name: "teams",
           link: "/admin/teams",
-          icon: <UsersFill className={iconStyle} />,
+          icon: <BsPeopleFill className={iconStyle} />,
         },
         {
           name: "judges",
           link: "/admin/judges",
-          icon: <Gavel className={iconStyle} />,
+          icon: <FaGavel className={iconStyle} />,
         },
         {
           name: "volunteers",
           link: "/admin/volunteers",
-          icon: <HandHeart className={iconStyle} />,
+          icon: <MdVolunteerActivism className={iconStyle} />,
         },
         {
           name: "mentors",
           link: "/admin/mentors",
-          icon: <HelpCircle className={iconStyle} />,
+          icon: <BiSolidHelpCircle className={iconStyle} />,
         },
         {
           name: "admin",
           link: "/admin/admins",
-          icon: <Lock className={iconStyle} />,
+          icon: <FaLock className={iconStyle} />,
         },
         {
           name: "committees",
           link: "/admin/committees",
-          icon: <Handshake className={iconStyle} />,
+          icon: <SiHandshake className={iconStyle} />,
         },
         {
           name: "sponsors",
           link: "/admin/sponsors",
-          icon: <Badge className={iconStyle} />,
+          icon: <GoSponsorTiers className={iconStyle} />,
         },
         {
           name: "panelists",
           link: "/admin/panelists",
-          icon: <People className={iconStyle} />,
+          icon: <IoIosPeople className={iconStyle} />,
         },
         {
           name: "interests",
           link: "/admin/interests",
-          icon: <AlertTriangle className={iconStyle} />,
+          icon: <FaExclamation className={iconStyle} />,
         },
         {
           name: "feedback",
           link: "/admin/feedback",
-          icon: <MessageSquare className={iconStyle} />,
+          icon: <MdFeedback className={iconStyle} />,
         },
         {
           name: "leads",
           link: "/admin/leads",
-          icon: <HandRaised className={iconStyle} />,
+          icon: <BsPersonRaisedHand className={iconStyle} />,
         },
       ],
     },
@@ -98,32 +99,32 @@ export const TABS = {
         {
           name: "calendar",
           link: "/admin/calendar",
-          icon: <Calendar className={iconStyle} />,
+          icon: <FaCalendarDay className={iconStyle} />,
         },
         {
           name: "contacts",
           link: "/admin/contacts",
-          icon: <Mail className={iconStyle} />,
+          icon: <FaEnvelope className={iconStyle} />,
         },
         {
           name: "check in",
           link: "/admin/checkin",
-          icon: <QrCode className={iconStyle} />,
+          icon: <AiOutlineQrcode className={iconStyle} />,
         },
         {
           name: "judging",
           link: "/admin/judging",
-          icon: <Gavel className={iconStyle} />,
+          icon: <FaGavel className={iconStyle} />,
         },
         {
           name: "statistics",
           link: "/admin/statistics",
-          icon: <BarChart2 className={iconStyle} />,
+          icon: <BsBarChartFill className={iconStyle} />,
         },
         {
           name: "settings",
           link: "/admin/settings",
-          icon: <Settings className={iconStyle} />,
+          icon: <FaGear className={iconStyle} />,
         },
       ],
     },
@@ -135,27 +136,27 @@ export const TABS = {
         {
           name: "dashboard",
           link: "/user/dashboard",
-          icon: <UserFill className={iconStyle} />,
+          icon: <BsFillPersonFill className={iconStyle} />,
         },
         {
           name: "checkin",
           link: "/user/checkin",
-          icon: <QrCode className={iconStyle} />,
+          icon: <AiOutlineQrcode className={iconStyle} />,
         },
         {
           name: "schedule",
           link: "/user/schedule",
-          icon: <Calendar className={iconStyle} />,
+          icon: <FaCalendarDay className={iconStyle} />,
         },
         {
           name: "team",
           link: "/user/team",
-          icon: <Users className={iconStyle} />,
+          icon: <RiTeamFill className={iconStyle} />,
         },
         {
           name: "find a team",
           link: "/user/find",
-          icon: <Search className={iconStyle} />,
+          icon: <FaSearch className={iconStyle} />,
         },
       ],
     },
--- src/data/admin/Icons.js
+++ src/data/admin/Icons.js
@@ -1,43 +1,39 @@
+import { IoIosMail, IoIosShirt, IoIosSchool } from "react-icons/io";
+import { SiGithub, SiDevpost, SiFigma } from "react-icons/si";
 import {
-  Mail,
-  Shirt,
-  School,
-  Github,
-  FileCode,
-  Figma,
-  Apple,
-  Phone,
-  Crown,
-  GraduationCap,
-  GenderMale,
-  BirthdayCake,
-  BookOpen,
-  Clock,
-  Contact,
-} from "lucide-react";
+  FaAppleAlt,
+  FaPhoneAlt,
+  FaCrown,
+  FaSchool,
+  FaVenusMars,
+  FaBirthdayCake,
+  FaBook,
+  FaClock,
+} from "react-icons/fa";
+import { MdContactPage } from "react-icons/md";
 
 export const ICONS = {
   github: (
-    <Github className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75" />
+    <SiGithub className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75" />
   ),
   devpost: (
-    <FileCode
-      /* No exact match to devpost*/ className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75"
-    />
+    <SiDevpost className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75" />
   ),
   figma: (
-    <Figma className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75" />
+    <SiFigma className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75" />
   ),
-  winner: <Crown className="ml-1 text-lg text-hackathon-yellow-100" />,
-  phone: <Phone className="mr-2 text-hackathon-blue-200" />,
-  email: <Mail className="mr-2 text-lg text-hackathon-blue-200" />,
-  shirt: <Shirt className="mr-2 text-lg text-hackathon-blue-200" />,
-  diet: <Apple className="mr-2 text-hackathon-blue-200" />,
-  age: <BirthdayCake className="mr-2 text-lg text-hackathon-blue-200" />,
-  gender: <GenderMale className="mr-2 text-lg text-hackathon-blue-200" />,
-  grade: <School className="mr-2 text-lg text-hackathon-blue-200" />,
-  major: <BookOpen className="mr-2 text-lg text-hackathon-blue-200" />,
-  school: <GraduationCap className="mr-2 text-lg text-hackathon-blue-200" />,
-  eventSource: <Contact className="mr-2 text-lg text-hackathon-blue-200" />,
-  priorHackathons: <Clock className="mr-2 text-lg text-hackathon-blue-200" />,
+  winner: <FaCrown className="ml-1 text-lg text-hackathon-yellow-100" />,
+  phone: <FaPhoneAlt className="mr-2 text-hackathon-blue-200" />,
+  email: <IoIosMail className="mr-2 text-lg text-hackathon-blue-200" />,
+  shirt: <IoIosShirt className="mr-2 text-lg text-hackathon-blue-200" />,
+  diet: <FaAppleAlt className="mr-2 text-hackathon-blue-200" />,
+  age: <FaBirthdayCake className="mr-2 text-lg text-hackathon-blue-200" />,
+  gender: <FaVenusMars className="mr-2 text-lg text-hackathon-blue-200" />,
+  grade: <IoIosSchool className="mr-2 text-lg text-hackathon-blue-200" />,
+  major: <FaBook className="mr-2 text-lg text-hackathon-blue-200" />,
+  school: <FaSchool className="mr-2 text-lg text-hackathon-blue-200" />,
+  eventSource: (
+    <MdContactPage className="mr-2 text-lg text-hackathon-blue-200" />
+  ),
+  priorHackathons: <FaClock className="mr-2 text-lg text-hackathon-blue-200" />,
 };
