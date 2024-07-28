102ccd064c5704c46e3405a88a5ec4c70b976db8
Andrew Khadder <andrewkh15@gmail.com>
2024-07-28 14:51:14 -0700

    fix import

--- src/components/user/find/Find.jsx
+++ src/components/user/find/Find.jsx
@@ -1,38 +1,71 @@
+"use client";
+
 import Title from "@/components/admin/Title";
 import Toolbar from "./Toolbar";
 import Idea from "./Idea";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { useRef } from "react";
+
+const generateDummyIdeas = (count) => {
+  const dummyIdeas = [];
+  for (let i = 1; i <= count; i++) {
+    dummyIdeas.push({
+      title: `Idea ${i}`,
+      technologies: ["Python", "TensorFlow"],
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      contact: "Please contact me on Discord using webdiv",
+    });
+  }
+  return dummyIdeas;
+};
 
 const Find = () => {
-  const ideas = [
-    {
-      title: "Idea 1",
-      technologies: ["Next.js", "PyTorch"],
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      contact: "Please contact me on Discord using webdiv",
+  // const ideas = [
+  //   {
+  //     title: "Idea 1",
+  //     technologies: ["Next.js", "PyTorch"],
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  //     contact: "Please contact me on Discord using webdiv",
+  //   },
+  //   {
+  //     title: "Idea 2",
+  //     technologies: ["Python", "TensorFlow"],
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  //     contact: "Please contact me on Discord using webdiv",
+  //   },
+  //   {
+  //     title: "Idea 3",
+  //     technologies: ["Python", "TensorFlow"],
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  //     contact: "Please contact me on Discord using webdiv",
+  //   },
+  //   {
+  //     title: "Idea 4",
+  //     technologies: ["Python", "TensorFlow"],
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  //     contact: "Please contact me on Discord using webdiv",
+  //   },
+  // ];
+
+  const ideas = generateDummyIdeas(75);
+
+  const containerRef = useRef(null);
+
+  const { measureElement, getVirtualItems } = useVirtualizer({
+    count: ideas.length,
+    getScrollElement: () => containerRef.current,
+    estimateSize: () => 30,
+    measureElement: (el) => {
+      if (el.clientHeight > 300) return 80;
+      else if (el.clientHeight > 200) return 60;
+      else return 30;
     },
-    {
-      title: "Idea 2",
-      technologies: ["Python", "TensorFlow"],
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      contact: "Please contact me on Discord using webdiv",
-    },
-    {
-      title: "Idea 3",
-      technologies: ["Python", "TensorFlow"],
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      contact: "Please contact me on Discord using webdiv",
-    },
-    {
-      title: "Idea 4",
-      technologies: ["Python", "TensorFlow"],
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      contact: "Please contact me on Discord using webdiv",
-    },
-  ];
+  });
 
   return (
     <div className="flex h-[calc(100vh-48px)] w-full flex-col">
@@ -40,16 +73,37 @@ const Find = () => {
         <Title title="Find a Team" />
       </div>
       <Toolbar />
-      <div className="mt-4 grid grid-cols-4 gap-4">
-        {ideas.map(({ title, technologies, description, contact }, index) => (
-          <Idea
-            key={index}
-            title={title}
-            technologies={technologies}
-            description={description}
-            contact={contact}
-          />
-        ))}
+      <div ref={containerRef} className="relative h-full overflow-y-scroll">
+        {getVirtualItems().map((virtualItem, index) => {
+          if (virtualItem.index % 4) return null;
+          const row = ideas.slice(index, index + 4);
+          return (
+            <div
+              key={`row: ${Math.floor(virtualItem.index / 4)}`}
+              className="grid grid-cols-4"
+              style={{
+                height: `${virtualItem.size}px`,
+                transform: `translateY(${virtualItem.start}px)`,
+              }}
+            >
+              {row.map((group, index) => (
+                <div
+                  key={`column: ${index}`}
+                  ref={measureElement}
+                  data-index={virtualItem.index + index}
+                  className="flex items-start p-2"
+                >
+                  <Idea
+                    title={group.title}
+                    technologies={group.technologies}
+                    description={group.description}
+                    contact={group.contact}
+                  />
+                </div>
+              ))}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
