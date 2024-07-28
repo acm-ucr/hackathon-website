4fb1350f01604195365d6cce06138af0f62e4242
Jonathan Le <jonathanle062303@gmail.com>
2024-07-28 14:25:52 -0700

    Change interfaces to types

--- src/components/Release.tsx
+++ src/components/Release.tsx
@@ -1,14 +1,14 @@
-interface ReleaseInterface {
+type ReleaseType = {
   START: Date;
   END: Date;
-}
+};
 
-interface Props {
-  release: ReleaseInterface;
+type props = {
+  release: ReleaseType;
   children: React.ReactNode;
-}
+};
 
-const Release = ({ release, children }: Props): React.ReactNode => {
+const Release = ({ release, children }: props): React.ReactNode => {
   if (process.env.NODE_ENV === "development") return children;
   return release.START < new Date() && new Date() < release.END && children;
 };
