10ba8d3409f5cea865a06f6490ae04246a8c3107
Anna <alee531@ucr.edu>
2024-07-31 22:29:23 -0700

    Altered Judging.jsx

--- src/data/user/Judging.jsx
+++ src/data/user/Judging.jsx
@@ -1,7 +1,6 @@
 export const JUDGING = [
   "Demos should be 2-3 minutes maximum",
   "All members need to be present when project is being judged",
-  "Submission must be through Github",
-  "All Github commits must occur within the hackathon period",
+  "All contributions must occur within the hackathon period",
   "Late projects will not be judged",
 ];
