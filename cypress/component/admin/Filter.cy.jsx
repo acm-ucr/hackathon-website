222d4116af043c0244a4634ef857766194abe7c1
shahdivyank <divyank.shah.2016@gmail.com>
2024-08-03 21:12:50 -0700

    fix tests

--- cypress/component/admin/Filter.cy.jsx
+++ cypress/component/admin/Filter.cy.jsx
@@ -2,47 +2,39 @@ import Filter from "@/components/admin/dashboards/dashboard/Filter";
 import React from "react";
 
 describe("Filter", () => {
-  const initialProps = {
+  const props = {
     value: "test",
     status: "Active",
     isActive: false,
   };
 
-  it("change colors", () => {
-    cy.mount(
-      <Filter {...initialProps} isActive={false} setFilters={() => {}} />,
-    );
+  it("Toggle", () => {
+    cy.mount(<Filter {...props} isActive={false} setFilters={() => {}} />);
     cy.get('[data-cy="Active-filter"]').should(
       "have.class",
-      "text-hackathon-blue-100 bg-white",
+      "bg-white text-hackathon-blue-100",
     );
-    cy.mount(
-      <Filter {...initialProps} isActive={true} setFilters={() => {}} />,
-    );
+    cy.mount(<Filter {...props} isActive={true} setFilters={() => {}} />);
     cy.get('[data-cy="Active-filter"]').should(
       "have.class",
-      "text-white bg-hackathon-blue-100",
+      "bg-hackathon-blue-100 text-white",
     );
   });
 
-  it("onClick setFilters", () => {
+  it("onClick", () => {
     const setFilters = cy.stub();
-    cy.mount(<Filter {...initialProps} setFilters={setFilters} />);
+    cy.mount(<Filter {...props} setFilters={setFilters} />);
     cy.get('[data-cy="Active-filter"]').click();
     cy.wrap(setFilters).should("have.been.calledOnce");
   });
 
-  it("TiPlus rotation", () => {
-    cy.mount(
-      <Filter {...initialProps} isActive={false} setFilters={() => {}} />,
-    );
+  it("Rotate", () => {
+    cy.mount(<Filter {...props} isActive={false} setFilters={() => {}} />);
     cy.get('[data-cy="Active-filter"] .duration-300').should(
       "not.have.class",
       "-rotate-45",
     );
-    cy.mount(
-      <Filter {...initialProps} isActive={true} setFilters={() => {}} />,
-    );
+    cy.mount(<Filter {...props} isActive={true} setFilters={() => {}} />);
     cy.get('[data-cy="Active-filter"] .duration-300').should(
       "have.class",
       "-rotate-45",
