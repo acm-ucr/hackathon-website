a981b4504e38577e7c2dca12df7c05eaecbb6663
Howardwheeler <howard.wheeler159@gmail.com>
2024-08-06 16:29:23 -0700

    made Icons Typescript

---
+++ src/data/admin/Icons.tsx
@@ -0,0 +1,61 @@
+import {
+  Apple,
+  Phone,
+  Crown,
+  School,
+  User,
+  Cake,
+  BookOpen,
+  Clock,
+  Mail,
+  Shirt,
+  Calendar,
+} from "lucide-react";
+
+import {
+  SiDevpost as Devpost,
+  SiGithub as Github,
+  SiFigma as Figma,
+} from "@icons-pack/react-simple-icons";
+
+interface icons {
+  github: JSX.Element;
+  devpost: JSX.Element;
+  figma: JSX.Element;
+  winner: JSX.Element;
+  phone: JSX.Element;
+  email: JSX.Element;
+  shirt: JSX.Element;
+  diet: JSX.Element;
+  age: JSX.Element;
+  gender: JSX.Element;
+  grade: JSX.Element;
+  major: JSX.Element;
+  school: JSX.Element;
+  eventSource: JSX.Element;
+  priorHackathons: JSX.Element;
+}
+
+export const ICONS: icons = {
+  github: (
+    <Github className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75" />
+  ),
+  devpost: (
+    <Devpost className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75" />
+  ),
+  figma: (
+    <Figma className="mr-2 text-lg hover:text-hackathon-green-300 hover:opacity-75" />
+  ),
+  winner: <Crown className="ml-1 text-lg text-hackathon-yellow-100" />,
+  phone: <Phone className="mr-2 text-hackathon-blue-200" />,
+  email: <Mail className="mr-2 text-lg text-hackathon-blue-200" />,
+  shirt: <Shirt className="mr-2 text-lg text-hackathon-blue-200" />,
+  diet: <Apple className="mr-2 text-hackathon-blue-200" />,
+  age: <Cake className="mr-2 text-lg text-hackathon-blue-200" />,
+  gender: <User className="mr-2 text-lg text-hackathon-blue-200" />,
+  grade: <School className="mr-2 text-lg text-hackathon-blue-200" />,
+  major: <BookOpen className="mr-2 text-lg text-hackathon-blue-200" />,
+  school: <School className="mr-2 text-lg text-hackathon-blue-200" />,
+  eventSource: <Calendar className="mr-2 text-lg text-hackathon-blue-200" />,
+  priorHackathons: <Clock className="mr-2 text-lg text-hackathon-blue-200" />,
+};
