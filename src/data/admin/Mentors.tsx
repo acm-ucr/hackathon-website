a5b2248c0083c64456e989d35e29a879b61eb279
Daniel <daniellee1807741@gmail.com>
2024-08-06 18:27:09 +0900

    new branch

--- src/data/admin/Mentors.tsx
+++ src/data/admin/Mentors.tsx
@@ -2,8 +2,30 @@ import Checkbox from "@/components/Checkbox";
 import { AVAILABILITY } from "../form/Information";
 import { generateSelect, generateStatus } from "./Columns";
 import { STATUSES } from "@/data/Statuses";
+import { ColumnDef } from "@tanstack/react-table";
 
-export const TAGS = [
+type Person = {
+  name: string;
+  email: string;
+  discord: string;
+  status: string | number;
+};
+
+type tags = {
+  text: string;
+  value: number;
+};
+
+type objectProp = {
+  availability: string[];
+  response: string;
+};
+
+type extendedColumnDef<TData, TValue> = ColumnDef<TData, TValue> & {
+  width: string;
+};
+
+export const TAGS: tags[] = [
   {
     text: "confirm",
     value: 1,
@@ -14,7 +36,7 @@ export const TAGS = [
   },
 ];
 
-export const COLUMNS = [
+export const COLUMNS: extendedColumnDef<Person, string>[] = [
   generateSelect(),
   {
     accessorKey: "name",
@@ -22,8 +44,7 @@ export const COLUMNS = [
     width: "w-3/12",
     enableColumnFilter: true,
     filterFn: "includesString",
-    searchable: true,
-    cell: ({ getValue }) => <div>{getValue()}</div>,
+    cell: (props: { getValue: () => string }) => <div>{props.getValue()}</div>,
   },
   {
     accessorKey: "email",
@@ -31,8 +52,7 @@ export const COLUMNS = [
     width: "w-4/12",
     enableColumnFilter: true,
     filterFn: "includesString",
-    searchable: true,
-    cell: ({ getValue }) => <div>{getValue()}</div>,
+    cell: (props: { getValue: () => string }) => <div>{props.getValue()}</div>,
   },
   {
     accessorKey: "discord",
@@ -40,13 +60,12 @@ export const COLUMNS = [
     width: "w-3/12",
     enableColumnFilter: true,
     filterFn: "includesString",
-    searchable: true,
-    cell: ({ getValue }) => <div>{getValue()}</div>,
+    cell: (props: { getValue: () => string }) => <div>{props.getValue()}</div>,
   },
   generateStatus(STATUSES),
 ];
 
-export const DROPDOWN = ({ object }) => {
+export const DROPDOWN = ({ object }: { object: objectProp }) => {
   return (
     <>
       <div className="flex justify-center">
@@ -56,6 +75,7 @@ export const DROPDOWN = ({ object }) => {
               toggle={object.availability.includes(text)}
               text={text}
               key={index}
+              color={"bg-hackathon-blue-100"}
             />
           ))}
         </div>
