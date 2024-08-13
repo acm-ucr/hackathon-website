3729de8534ffb78b3ea9f746403de0e78490b8ea
Taah <admin@taah.dev>
2024-08-13 05:14:20 -0700

    Migrate admins dashboard to typescript

--- src/data/admin/Admins.tsx
+++ src/data/admin/Admins.tsx
@@ -2,8 +2,44 @@ import { generateAffiliation, generateSelect, generateStatus } from "./Columns";
 import { AFFILIATIONS } from "../form/Information";
 import { ICONS } from "./Icons";
 import { STATUSES } from "../Statuses";
+import {
+  AccessorColumnDef,
+  DisplayColumnDef,
+  GroupColumnDef,
+  RowData,
+} from "@tanstack/react-table";
 
-export const TAGS = [
+type TagText = "accept" | "reject";
+type TagCode = 1 | -1;
+
+type Tag = {
+  text: TagText;
+  value: TagCode;
+};
+
+type Admin = {
+  name: string;
+  email: string;
+  affiliation: string;
+  discord: string;
+  major: string;
+  grade: string;
+  gender: string;
+  shirt: string;
+};
+
+type SearchableColumnDef<TData extends RowData, TValue = unknown> =
+  | DisplayColumnDef<TData, TValue>
+  | GroupColumnDef<TData, TValue>
+  | (AccessorColumnDef<TData, TValue> & {
+      searchable: boolean;
+    });
+
+type dropdownProps = {
+  object: Record<string, string[] | string>;
+};
+
+export const TAGS: Tag[] = [
   {
     text: "accept",
     value: 1,
@@ -14,7 +50,7 @@ export const TAGS = [
   },
 ];
 
-export const COLUMNS = [
+export const COLUMNS: SearchableColumnDef<Admin, string>[] = [
   generateSelect(),
   {
     accessorKey: "name",
@@ -23,7 +59,7 @@ export const COLUMNS = [
     enableColumnFilter: true,
     filterFn: "includesString",
     searchable: true,
-    cell: ({ getValue }) => <div>{getValue()}</div>,
+    cell: (props: { getValue: () => string }) => <div>{props.getValue()}</div>,
   },
   {
     accessorKey: "email",
@@ -32,7 +68,7 @@ export const COLUMNS = [
     enableColumnFilter: true,
     filterFn: "includesString",
     searchable: true,
-    cell: ({ getValue }) => <div>{getValue()}</div>,
+    cell: (props: { getValue: () => string }) => <div>{props.getValue()}</div>,
   },
   {
     accessorKey: "discord",
@@ -41,13 +77,13 @@ export const COLUMNS = [
     enableColumnFilter: true,
     filterFn: "includesString",
     searchable: true,
-    cell: ({ getValue }) => <div>{getValue()}</div>,
+    cell: (props: { getValue: () => string }) => <div>{props.getValue()}</div>,
   },
   generateAffiliation(AFFILIATIONS),
   generateStatus(STATUSES),
 ];
 
-const attributes = [
+const attributes: string[] = [
   "email",
   "phone",
   "age",
@@ -60,11 +96,11 @@ const attributes = [
   "restriction",
 ];
 
-export const DROPDOWN = ({ object }) => {
+export const DROPDOWN: React.FC<dropdownProps> = ({ object }) => {
   return (
     <div className="flex items-center justify-center">
       <div className="grid w-11/12 grid-cols-3">
-        {attributes.map((attribute, index) => (
+        {attributes.map((attribute: string, index: number) => (
           <div key={index} className="my-1 flex px-1 text-sm">
             {ICONS[attribute]}
             {Array.isArray(object[attribute])
