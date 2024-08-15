b68dfe544274973ec41abceb4ca5a209e3797bfb
nidheesh-m-vakharia <nvakh003@ucr.edu>
2024-08-14 22:40:54 -0700

    types implimented

--- src/data/form/Mentors.ts
+++ src/data/form/Mentors.ts
@@ -1,7 +1,45 @@
+import {
+  CheckboxInput,
+  Description,
+  RadioInput,
+  SelectInput,
+  TermsAndConditions,
+  TextareaInput,
+  TextInput,
+} from "@/types/forms";
 import { MAJORS, GRADES, GENDERS, SHIRTS, AVAILABILITY } from "./Information";
 import data from "@/data/Config";
 
-export const FIELDS = {
+type Feilds = {
+  description: Description;
+  name: TextInput;
+  email: TextInput;
+  phone: TextInput;
+  discord: TextInput;
+  major: SelectInput;
+  grade: SelectInput;
+  availability: CheckboxInput;
+  gender: RadioInput;
+  shirt: RadioInput;
+  response: TextareaInput;
+  requirements: TermsAndConditions;
+};
+
+type Attributes = {
+  name: string;
+  email: string;
+  phone: string;
+  discord: string;
+  major: string;
+  grade: string;
+  availability: string[];
+  gender: string;
+  shirt: string;
+  response: string;
+  requirements: string[];
+};
+
+export const FIELDS: Feilds = {
   description: {
     input: "description",
     width: 12,
@@ -31,6 +69,7 @@ export const FIELDS = {
     width: 12,
     editable: false,
     required: true,
+    placeholder: "John Doe",
   },
   email: {
     input: "input",
@@ -41,6 +80,7 @@ export const FIELDS = {
     width: 12,
     editable: false,
     required: true,
+    placeholder: "john.doe@gmail.com",
   },
   phone: {
     input: "input",
@@ -51,6 +91,7 @@ export const FIELDS = {
     maxLength: 50,
     width: 12,
     required: true,
+    editable: true,
   },
   discord: {
     input: "input",
@@ -61,6 +102,7 @@ export const FIELDS = {
     maxLength: 50,
     width: 12,
     required: true,
+    editable: true,
   },
   major: {
     input: "select",
@@ -71,6 +113,7 @@ export const FIELDS = {
     width: 12,
     required: true,
     searchable: true,
+    editable: true,
   },
   grade: {
     input: "select",
@@ -81,6 +124,7 @@ export const FIELDS = {
     width: 12,
     required: true,
     searchable: true,
+    editable: true,
   },
   availability: {
     input: "checkboxes",
@@ -89,6 +133,7 @@ export const FIELDS = {
     text: "Availability",
     required: true,
     options: AVAILABILITY,
+    editable: true,
   },
   gender: {
     input: "radio",
@@ -97,6 +142,7 @@ export const FIELDS = {
     field: "gender",
     width: 12,
     required: true,
+    editable: true,
   },
   shirt: {
     input: "radio",
@@ -105,6 +151,7 @@ export const FIELDS = {
     field: "shirt",
     width: 12,
     required: true,
+    editable: true,
   },
   response: {
     input: "textarea",
@@ -114,6 +161,7 @@ export const FIELDS = {
     placeholder: "ie. I can bring...",
     width: 12,
     required: true,
+    editable: true,
   },
   requirements: {
     text: "Terms and Conditions",
@@ -121,6 +169,7 @@ export const FIELDS = {
     width: 12,
     field: "requirements",
     required: true,
+    editable: true,
     options: [
       "I have read the MLH code of conduct and agree to the terms and conditions listed",
       "I authorize you to share my application/registration information with Major League Hacking for event administration, ranking, and MLH administration in-line with the MLH Privacy Policy",
@@ -134,7 +183,7 @@ export const FIELDS = {
   },
 };
 
-export const ATTRIBUTES = {
+export const ATTRIBUTES: Attributes = {
   name: "",
   email: "",
   phone: "",
