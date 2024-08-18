94931efb21d39a94aab3b8572a6f365a8b0c8248
Amir <nintendothing123@gmail.com>
2024-08-17 23:53:59 -0700

    change layout.js to ts

---
+++ src/app/engineering/layout.tsx
@@ -0,0 +1,20 @@
+import { SiTailwindcss } from "@icons-pack/react-simple-icons";
+import Navigation from "@/components/engineering/Navigation";
+import React, {ReactNode} from 'react';
+
+
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="w-full">
+      < Navigation />
+      <div className="mt-20">{ children }</div>
+    </div>
+  );
+};
+
+export default Layout;
