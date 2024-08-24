9e3e1226bfb7a22a6697ca5fdab47e4c904f569f
Amir <nintendothing123@gmail.com>
2024-08-23 19:54:51 -0700

    changed email.js to email.ts

--- src/utils/email.tsx
+++ src/utils/email.tsx
@@ -1,13 +1,31 @@
 import Email from "@/components/email/Email";
 import { Resend } from "resend";
 
+interface SendEmailParams {
+  email: string;
+  id: string;
+  name: string;
+  position: string;
+  subject: string;
+  preview: string;
+}
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const send = async ({ email, id, name, position, subject, preview }) => {
+const send = async ({
+  email,
+  id,
+  name,
+  position,
+  subject,
+  preview,
+}: SendEmailParams) => {
   const { data, error } = await resend.emails.send({
     from: "Hackathon <info@hackathon.com>",
     to: [email],
     subject: subject,
+    text: `Dear ${name},\n\ Here is a preview to where to meet \n\ Happy Hacking
+    \n\, Hackathon Team `,
     // eslint-disable-next-line new-cap
     react: Email({ id, name, position, preview }),
   });
