c7632dfad18a3bcb2dd63fbc84e6b3823ab4c037
Amir <nintendothing123@gmail.com>
2024-08-29 15:03:55 -0700

    fixed

--- src/data/admin/Feedback.tsx
+++ src/data/admin/Feedback.tsx
@@ -1,11 +1,14 @@
+import { CellContext, ColumnDef } from "@tanstack/react-table";
 import { generateSelect, generateStatus } from "./Columns";
+import { Tags } from "@/types/dashboard";
+import Interest from "@/components/form/Interest";
 
 export const STATUSES = {
   1: "read",
   0: "unread",
 };
 
-export const TAGS = [
+export const TAGS: Tags[] = [
   {
     text: "read",
     value: 1,
@@ -16,35 +19,46 @@ export const TAGS = [
   },
 ];
 
-interface CellProp {
-  getValue: () => any;
-}
+type Interest = {
+  rating: string;
+  eventSource: string;
+  improvements: string;
+  helpful: string;
+};
 
-export const COLUMNS = [
+export const COLUMNS: ColumnDef<Interest, string>[] = [
   generateSelect(),
   {
     accessorKey: "rating",
     header: "Rating",
     meta: { width: "w-[8%]" },
-    cell: ({ getValue }: CellProp) => <div>{getValue()}</div>,
+    cell: (props: CellContext<Interest, Interest["rating"]>) => (
+      <div>{props.getValue()}</div>
+    ),
   },
   {
     accessorKey: "eventSource",
     header: "Event Source",
     meta: { width: "w-[15%]" },
-    cell: ({ getValue }: CellProp) => <div>{getValue()}</div>,
+    cell: (props: CellContext<Interest, Interest["eventSource"]>) => (
+      <div>{props.getValue()}</div>
+    ),
   },
   {
     accessorKey: "improvements",
     header: "Improvements",
     meta: { width: "w-[31%]" },
-    cell: ({ getValue }: CellProp) => <div>{getValue()}</div>,
+    cell: (props: CellContext<Interest, Interest["improvements"]>) => (
+      <div>{props.getValue()}</div>
+    ),
   },
   {
     accessorKey: "helpful",
     header: "Helpful",
     meta: { width: "w-[31%]" },
-    cell: ({ getValue }: CellProp) => <div>{getValue()}</div>,
+    cell: (props: CellContext<Interest, Interest["helpful"]>) => (
+      <div>{props.getValue()}</div>
+    ),
   },
   generateStatus(STATUSES),
 ];
