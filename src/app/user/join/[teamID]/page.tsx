6f0ecb9ec1578b3ba608404356191dbe4d7e7388
Rama Chunduri <chunduri156@gmail.com>
2024-09-02 20:11:27 -0400

    user teams

---
+++ src/app/user/join/[teamID]/page.tsx
@@ -0,0 +1,25 @@
+import ProtectedPage from "@/components/ProtectedPage";
+import Join from "@/components/user/join/Join";
+
+type teamIDProps = {
+  teamID: string;
+};
+
+type props = {
+  params: teamIDProps;
+};
+
+const Page = ({ params }: props) => {
+  return (
+    <ProtectedPage
+      title="User | Join"
+      restrictions={{
+        participants: [1],
+      }}
+    >
+      <Join params={params} />
+    </ProtectedPage>
+  );
+};
+
+export default Page;
