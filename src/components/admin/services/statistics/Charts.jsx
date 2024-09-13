58639ccc29da07382733a62263197b7ce7333988
py1u.dev <py1u.dev@gmail.com>
2024-09-12 17:14:46 -0700

    refactor: remove top margin spacing for Tabs.jsx and Charts.jsx

--- src/components/admin/services/statistics/Charts.jsx
+++ src/components/admin/services/statistics/Charts.jsx
@@ -2,7 +2,7 @@ import Chart from "./Chart";
 
 const Charts = ({ counts }) => {
   return (
-    <div className="mt-3 grid w-full grid-cols-2 gap-4 p-4 md:grid-cols-4">
+    <div className="grid w-full grid-cols-2 gap-4 p-4 md:grid-cols-4">
       {Object.entries(counts).map(([title, data], index) => (
         <Chart key={index} title={title} data={data} />
       ))}
