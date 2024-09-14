f8917714f3e3ffc8a25dfef8454766a22ef929c1
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-09-13 17:01:49 -0700

    reduced to 2 lines

--- src/app/visualize/page.tsx
+++ src/app/visualize/page.tsx
@@ -1,11 +1,2 @@
-import React from "react";
 import Visualizer from "next-route-visualizer";
-const page = () => {
-  return (
-    <div>
-      <Visualizer />
-    </div>
-  );
-};
-
-export default page;
+export default Visualizer;
