f8f89d4a95ed051213a0f8d4622cfceedf7feb2f
Derrick-Mao <derrickmao03@gmail.com>
2024-09-15 00:26:41 -0700

    format

--- src/components/admin/Tag.jsx
+++ src/components/admin/Tag.jsx
@@ -4,10 +4,9 @@ const Tag = ({ color, text, onClick, classes, statuses }) => {
   return (
     <Badge
       data-cy={`${text}-tag`}
-      className={`m-0 w-fit whitespace-nowrap rounded px-2 py-0.5 text-xs md:text-sm
-        ${classes} ${color.background} ${color.text} ${
-          onClick && `hover:cursor-pointer ${color.hover}`
-        }`}
+      className={`m-0 w-fit whitespace-nowrap rounded px-2 py-0.5 text-xs md:text-sm ${classes} ${color.background} ${color.text} ${
+        onClick && `hover:cursor-pointer ${color.hover}`
+      }`}
       onClick={onClick}
     >
       {isNaN(text) ? text : statuses[text]}
