2937494c4b0b96577c28064c3c958eb12306e53d
Amir Shanti <amirvs9675@gmail.com>
2024-09-17 02:02:38 -0700

    last one fs this time

--- src/components/user/resources/Hackpack.tsx
+++ src/components/user/resources/Hackpack.tsx
@@ -11,39 +11,37 @@ type props = {
 
 const Hackpack = ({ text, techs, link, description }: props) => {
   return (
-    <div className="rounded-xl bg-white p-3">
-      <Link
-        data-cy="hackpack-link"
-        className="items-center justify-between opacity-100 hover:opacity-70"
-        href={link}
-        target="_black"
+    <Link
+      data-cy="hackpack-link"
+      className="items-center justify-between rounded-xl bg-white p-3 opacity-100 hover:opacity-70"
+      href={link}
+      target="_black"
+    >
+      <div
+        className="flex items-center justify-between text-lg font-semibold"
+        data-cy="hackpack-text"
       >
-        <div
-          className="flex items-center justify-between text-lg font-semibold"
-          data-cy="hackpack-text"
-        >
-          {text}
-          <div>
-            <Github size={20} />
-          </div>
+        {text}
+        <div>
+          <Github size={20} />
         </div>
-        <div className="my-2 flex flex-wrap gap-2" data-cy="hackpack-techs">
-          {techs.map((tech, index) => (
-            <div
-              key={index}
-              className="flex items-center gap-2 text-gray-400"
-              data-cy="hackpack-tech"
-            >
-              <div className="text-hackathon-blue-100" data-cy="hackpack-icon">
-                {TECHSTACKS[tech]}
-              </div>
-              {tech}
+      </div>
+      <div className="my-2 flex flex-wrap gap-2" data-cy="hackpack-techs">
+        {techs.map((tech, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-2 text-gray-400"
+            data-cy="hackpack-tech"
+          >
+            <div className="text-hackathon-blue-100" data-cy="hackpack-icon">
+              {TECHSTACKS[tech]}
             </div>
-          ))}
-        </div>
-        <p className="mt-2">{description}</p>
-      </Link>
-    </div>
+            {tech}
+          </div>
+        ))}
+      </div>
+      <p className="mt-2">{description}</p>
+    </Link>
   );
 };
 
