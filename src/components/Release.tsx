c552b3b20d0289ea84a49e15c3f8c4d151a23867
Amir <nintendothing123@gmail.com>
2024-09-17 23:33:28 -0700

    changed page.js to ts

--- src/components/Release.tsx
+++ src/components/Release.tsx
@@ -4,12 +4,17 @@ type ReleaseType = {
 };
 
 type props = {
-  release: ReleaseType;
+  release: ReleaseType | Date;
   children: React.ReactNode;
 };
 
 const Release = ({ release, children }: props): React.ReactNode => {
   if (process.env.NODE_ENV === "development") return children;
+
+  if (release instanceof Date) {
+    return release < new Date() ? children : null;
+  }
+
   return release.START < new Date() && new Date() < release.END && children;
 };
 
