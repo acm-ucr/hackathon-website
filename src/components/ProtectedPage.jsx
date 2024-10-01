eadddeede5b24af8a3492a13eefae42f3b38c53e
seanquiambao <slquiambao03@gmail.com>
2024-09-30 17:30:33 -0700

    removed comments and console logs

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -26,7 +26,6 @@ const ProtectedPage = async ({ children, restrictions, title }) => {
     throw new Fault(403, "Unauthorized", "You do not have any assigned roles");
   }
 
-  console.log("session:", session.user.roles);
   const authorized = Object.entries(restrictions).some(([key, values]) =>
     Array.isArray(values)
       ? values.includes(session.user.roles[key])
@@ -51,56 +50,3 @@ const ProtectedPage = async ({ children, restrictions, title }) => {
 };
 
 export default ProtectedPage;
-
-// delete later
-// useEffect(() => {
-//   if (RELEASES[pathName] > new Date()) {
-//     throw new Fault(
-//       423,
-//       "Locked Resource",
-//       "This resource has not been released",
-//     );
-//   }
-
-//   if (status === "loading") return;
-//   if (status !== "authenticated") {
-//     void signIn("google");
-//     return;
-//   }
-
-//   if (!session.user.roles && Object.keys(restrictions).length > 0) {
-//     throw new Fault(
-//       403,
-//       "Unauthorized",
-//       "You do not have any assigned roles",
-//     );
-//   }
-
-//   const authorized = Object.entries(restrictions).some(([key, values]) =>
-//     Array.isArray(values)
-//       ? values.includes(session.user.roles[key])
-//       : session.user.roles[key] === values,
-//   );
-
-//   if (!authorized && Object.keys(restrictions).length > 0) {
-//     throw new Fault(403, "Unauthorized", "You do not have access this page");
-//   }
-//   setConfirmed(true);
-// }, [status]);
-
-// const navigation = RegExp(/user\/|admin\//).test(pathName);
-
-// return (
-//   <>
-//     {status === "loading" && <Loading />}
-//     {confirmed && (
-//       <>
-//         <title>{title}</title>
-//         {navigation && <Navigation />}
-//         <div className="z-0 flex h-screen w-full items-start justify-center overflow-x-hidden bg-hackathon-page py-12 lg:py-0">
-//           <div className="h-full w-11/12">{children}</div>
-//         </div>
-//       </>
-//     )}
-//   </>
-// );
